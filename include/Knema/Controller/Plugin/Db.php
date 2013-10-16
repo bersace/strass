a62da23e2dc2a45ce3d13dac1af829488e1e50ea
Étienne BERSAC <bersace03@gmail.com>
2013-10-16 17:04:43 +0200

    Déplacer le cache dans private/

--- include/Knema/Controller/Plugin/Db.php
+++ include/Knema/Controller/Plugin/Db.php
@@ -12,7 +12,7 @@ class Knema_Controller_Plugin_Db extends Zend_Controller_Plugin_Abstract
 		Zend_Db_Table_Abstract::setDefaultAdapter($db);
 		$cache = Zend_Cache::factory('Core', 'File',
 					     array('automatic_serialization' => true),
-					     array('cache_dir' => 'cache'));
+					     array('cache_dir' => 'private/cache'));
 		Zend_Registry::set('cache', $cache);
 		Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
 		Zend_Registry::set('db', $db);
