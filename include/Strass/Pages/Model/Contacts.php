98d8697176493ce67aaad8d97deb16b81db30bf5
Étienne BERSAC <bersace03@gmail.com>
2013-12-08 21:52:06 +0100

    Unifier le modèle de la paginations des contacts avec l'accueil

--- include/Strass/Pages/Model/Contacts.php
+++ include/Strass/Pages/Model/Contacts.php
@@ -1,24 +1,13 @@
 <?php
 
-class Strass_Pages_Model_Contacts extends Wtk_Pages_Model_Assoc
+class Strass_Pages_Model_Contacts extends Strass_Pages_Model_Historique
 {
-  protected $_unite;
-  protected $_cette_annee;
-  protected $_calendrier;
-
-  function __construct(Unite $unite, $annee)
-  {
-    $this->_unite = $unite;
-
-    parent::__construct($unite->getAnneesOuverte(), $annee);
-  }
-
   function fetch($annee = null)
   {
-    $apps = $this->_unite->getApps($annee);
+    $apps = $this->unite->getApps($annee);
 
     // de même pour les sous-unités
-    $sousunites = $this->_unite->getSousUnites(false, $annee);
+    $sousunites = $this->unite->getSousUnites(false, $annee);
     $ssapps = array();
     foreach($sousunites as $su) {
       switch($su->type) {
@@ -33,20 +22,11 @@ class Strass_Pages_Model_Contacts extends Wtk_Pages_Model_Assoc
 	break;
       }
     }
-    return array('unite' => $this->_unite,
+
+    return array('unite' => $this->unite,
 		 'sousunites' => $sousunites,
 		 'apps' => $apps,
 		 'sousapps' => $ssapps,
 		 );
-  }
-
-  protected function isActuelle($annee)
-  {
-    return $annee == $this->_cette_annee;
-  }
-
-  function valid()
-  {
-    return in_array($this->pointer, $this->pages_id);
   }
 }
