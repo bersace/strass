9dbf86f311aa7f02a1c15ae6e73634904b1dd6de
Étienne BERSAC <bersace03@gmail.com>
2013-12-08 15:22:36 +0100

    Utiliser comme base le thème suf1520

--- data/styles/strass/metas.php
+++ data/styles/strass/metas.php
@@ -1,3 +1,3 @@
 <?php
-return new Wtk_Metas(array('author' 	=> 'Arnaud de Ruffray, Étienne Bersac',
-			   'title'	=> 'Santiago'));
+return new Wtk_Metas(array('author' 	=> 'Étienne Bersac',
+			   'title'	=> 'Uniforme SUF'));
