701331fbd5c2d9ad1fcff3f958084a3963f7b9eb
Étienne BERSAC <bersace03@gmail.com>
2013-12-23 03:53:55 +0100

    Ne pas ajouter deux fois l'année dans la branche d'une photo

--- include/Strass/Controller/Action/Helper/Photo.php
+++ include/Strass/Controller/Action/Helper/Photo.php
@@ -14,15 +14,6 @@ class Strass_Controller_Action_Helper_Photo extends Strass_Controller_Action_Hel
 		if (!$p && $throw)
 			throw new Zend_Controller_Exception("Photo invalide");
 
-		$annee = $a->getAnnee();
-		$this->_actionController->branche->insert(-1, $annee,
-							  array('controller' => 'photos',
-								'action' => 'index',
-								'annee' => $annee),
-							  array(),
-							  true);
-
-
 		if ($p)
 			$this->_actionController->branche->append(wtk_ucfirst($p->titre),
 								  array('controller'	=> 'photos',
