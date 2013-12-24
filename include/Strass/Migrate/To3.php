a3477e4ed7a5aa94ee59269dd82ebe1f8313cf52
Étienne BERSAC <bersace03@gmail.com>
2013-12-24 18:13:59 +0100

    Ajout d'une clef primaire entières à la table des citations

--- include/Strass/Migrate/To3.php
+++ include/Strass/Migrate/To3.php
@@ -2,6 +2,18 @@
 
 class Strass_Migrate_To3 extends Strass_MigrateHandler {
   function online($db) {
-    $db->exec("ALTER TABLE citations RENAME TO citation;");
+    $db->exec("ALTER TABLE citations RENAME TO citation_tmp;");
+    $db->exec("
+CREATE TABLE citation (
+id INTEGER PRIMARY KEY,
+texte TEXT,
+auteur CHAR(128),
+date CHAR(16),
+posteur CHAR(128)
+);");
+    $db->exec("
+INSERT INTO citation (texte, auteur, date)
+SELECT citation, auteur, date FROM citation_tmp ORDER BY date;");
+    $db->exec("DROP TABLE citation_tmp;");
   }
 }
