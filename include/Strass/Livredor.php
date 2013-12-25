4d04273ce151d6c435282e5dcc05884e50850d51
Étienne BERSAC <bersace03@gmail.com>
2013-12-25 15:35:22 +0100

    Ajouter une colonne id aux messages du livre d'or

--- include/Strass/Livredor.php
+++ include/Strass/Livredor.php
@@ -8,7 +8,6 @@ class Livredor extends Strass_Db_Table_Abstract implements Zend_Acl_Resource_Int
 	function __construct($config = array())
 	{
 		parent::__construct($config);
-		// utiliser DI pour singleton ?
 		$acl = Zend_Registry::get('acl');
 		if (!$acl->has($this)) {
 			$acl->add($this);
@@ -17,7 +16,6 @@ class Livredor extends Strass_Db_Table_Abstract implements Zend_Acl_Resource_Int
 
 	function getResourceId()
 	{
-		// suffixer avec knema/site/id ?
 		return 'livredor';
 	}
 }
@@ -26,7 +24,6 @@ class Message extends Zend_Db_Table_Row_Abstract implements Zend_Acl_Resource_In
 {
 	function getResourceId()
 	{
-		// suffixer avec knema/site/id ?
-		return 'livredor';
+		return 'livredor-message-'.$this->id;
 	}
 }
---
+++ include/Strass/Migrate/To5.php
@@ -0,0 +1,20 @@
+<?php
+
+class Strass_Migrate_To5 extends Strass_MigrateHandler {
+  function online($db) {
+    $db->exec("ALTER TABLE livredor RENAME TO tmp;");
+    $db->exec("
+CREATE TABLE `livredor` (
+       id INTEGER PRIMARY KEY,
+       auteur           CHAR(128),
+       adelec           CHAR(128),
+       date             CHAR(16),
+       public           CHAR(1) DEFAULT NULL,
+       message          TEXT    NOT NULL
+);");
+    $db->exec("
+INSERT INTO livredor (auteur, adelec, date, public, message)
+SELECT auteur, adelec, date, public, message FROM tmp ORDER BY date;");
+    $db->exec("DROP TABLE tmp;");
+  }
+}
