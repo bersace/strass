2da35d8dc6b22113eae99512a488be0b19e1aef2
Étienne BERSAC <bersace03@gmail.com>
2013-12-27 04:57:05 +0100

    Ne gérer que index.php avec le serveur interne de PHP

--- devel.php
+++ devel.php
@@ -3,7 +3,7 @@
 // Contournement de bug dans le serveur embarqué de PHP
 $_SERVER['SCRIPT_NAME'] = substr($_SERVER['SCRIPT_FILENAME'], strlen($_SERVER['DOCUMENT_ROOT']));
 
-// Ne servir que index.php
-if (basename($_SERVER['SCRIPT_FILENAME']) != 'index.php') return false;
+// Ne servir que les requêtes PATH_INFO
+if (file_exists('./'.$_SERVER['REQUEST_URI'])) return false;
 
-include_once $_SERVER['SCRIPT_FILENAME'];
+include_once 'index.php';
