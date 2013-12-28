f4116bfc3be3da198bb4f87e5209b024f546043e
Étienne BERSAC <bersace03@gmail.com>
2013-12-28 02:01:13 +0100

    Codes par défaut pour des exceptions
    Pas vraiment testé. C'est juste qu'il faut vraiment améliorer la gestion
    des erreurs.

--- include/Strass/Controller/Action/Exception.php
+++ include/Strass/Controller/Action/Exception.php
@@ -1,5 +1,10 @@
 <?php
 
-class Strass_Controller_Action_Exception extends Exception
-{}
-
+class Strass_Controller_Action_Exception extends Zend_Controller_Action_Exception
+{
+  function __construct($message=null, $code=500)
+  {
+    $args = func_get_args();
+    call_user_func_array(array('parent', '__construct'), $args);
+  }
+}
--- include/Strass/Controller/Action/Exception/Forbidden.php
+++ include/Strass/Controller/Action/Exception/Forbidden.php
@@ -1,5 +1,10 @@
 <?php
 
 class Strass_Controller_Action_Exception_Forbidden extends Strass_Controller_Action_Exception
-{}
-
+{
+  function __construct($message=null, $code=403)
+  {
+    $args = func_get_args();
+    call_user_func_array(array('parent', '__construct'), $args);
+  }
+}
