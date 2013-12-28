863a701eb0e8eb11de42ea1404bfdc058851bfd3
Étienne BERSAC <bersace03@gmail.com>
2013-12-28 02:02:13 +0100

    Afficher moins de traces à l'utilisateur

--- include/Strass/Views/Scripts/error/error.wtk.php
+++ include/Strass/Views/Scripts/error/error.wtk.php
@@ -1,50 +1,50 @@
 <?php
-  // simple listage des erreurs.
+// simple listage des erreurs.
 if (count($this->errors > 1)) {
-    $this->document->setTitle("Erreurs");
-  }
+  $this->document->setTitle("Erreurs");
+}
 
 foreach ($this->errors as $i => $error) {
-	if ($error instanceof Strass_Controller_Action_Exception_Forbidden)
-		$titre = "Accès refusé";
-	else if ($error instanceof Strass_Controller_Action_Exception_Notice)
-		$titre = "Notice";
-	else
-		$titre = "Erreur".(count($this->errors)>1 ? " #".($i+1) : "");
+  if ($error instanceof Strass_Controller_Action_Exception_Forbidden)
+    $titre = "Accès refusé";
+  else if ($error instanceof Strass_Controller_Action_Exception_Notice)
+    $titre = "Notice";
+  else
+    $titre = "Erreur".(count($this->errors)>1 ? " #".($i+1) : "");
 
-	if (count($this->errors) == 1) {
-	  $this->document->setTitle($titre);
-	  $titre = null;
-	}
+  $this->document->setTitle(null);
 
-	$section = $this->document->addSection("error".$i, $titre);
-	$section->addChild (new Wtk_Text ("// ".$error->getMessage()." // \n"));
+  $section = $this->document->addDialog($titre)
+    ->setId("error".$i)
+    ->addFlags('error');
+  $section->addText("// ".$error->getMessage()." // \n");
 
-	if ($error instanceof Strass_Controller_Action_Exception_Forbidden) {
-		if (Zend_Registry::get('user')) {
-			$config = new Strass_Config_Php('strass');
-			$section->addText("Si vous devriez avoir accès au site, ".
-					  "[mailto:".$config->site->admin." contactez le webmestre].");
-		}
-		else {
-			$section->addParagraph("Si vous êtes inscrit, identifiez-vous. Sinon, ",
-					       $this->lien(array('controller' => 'membres',
-								 'action' => 'inscription'),
-							   "inscrivez-vous",
-							   true),
-					       ".");
-		}			
-	}
-
-	if (!($error instanceof Strass_Controller_Action_Exception)) {
-		$section->addText("à {{".$error->getFile().":".$error->getLine()."}}\n");
-		$backtrace = $section->addSection("backtrace".$i, "Backtrace");
-		$list = $backtrace->addChild(new Wtk_List(true));
-		foreach ($error->getTrace() as $step) {
-			extract($step);
-			$list->addChild(new Wtk_Text((isset($file) ? $file.":".$line." " : "")." ".
-						     (isset($class) ? $class."::" : "").$step['function']));
-		}
-	}
+  if ($error instanceof Strass_Controller_Action_Exception_Forbidden) {
+    if (Zend_Registry::get('user')) {
+      $config = new Strass_Config_Php('strass');
+      $section->addText("Si vous devriez avoir accès au site, ".
+			"[mailto:".$config->site->admin." contactez le webmestre].");
+    }
+    else {
+      $section->addParagraph("Si vous êtes inscrit, identifiez-vous. Sinon, ",
+			     $this->lien(array('controller' => 'membres',
+					       'action' => 'inscription'),
+					 "inscrivez-vous", true),
+			     ".");
+    }
   }
 
+  $d = $section->addSection(null, 'Détails')->addFlags('details');
+  $d->addText("à {{".$error->getFile().":".$error->getLine()."}}\n");
+  $backtrace = $d->addSection("backtrace".$i, "Backtrace")->addFlags('trace');
+  $list = $backtrace->addList()->setOrdered()->setReversed();
+  foreach ($error->getTrace() as $step) {
+    extract($step);
+    $list->addItem()->addText((isset($file) ? $file.":".$line." " : "")." ".
+			      (isset($class) ? $class."::" : "").$step['function']);
+  }
+
+  if (!($error instanceof Zend_Controller_Action_Exception)) {
+    $section->addFlags('showtrace');
+  }
+}
