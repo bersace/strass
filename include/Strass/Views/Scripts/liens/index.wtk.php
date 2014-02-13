852b7bf35fdb543849ae1cfb527eae0978b43718
Étienne BERSAC <bersace03@gmail.com>
2014-02-13 17:15:32 +0100

    Afficher un bouchon lorsqu'il n'y a pas de liens

--- include/Strass/Views/Scripts/liens/index.wtk.php
+++ include/Strass/Views/Scripts/liens/index.wtk.php
@@ -1,7 +1,13 @@
 <?php
-$l = $this->document->addList();
-foreach($this->liens as $lien) {
-  $i = $l->addItem();
-  $i->addParagraph()->addLink($lien->url, $lien->nom);
-  $i->addParagraph()->addFlags('description')->addInline($lien->description);
+if ($this->liens->count()) {
+  $l = $this->document->addList();
+  foreach($this->liens as $lien) {
+    $i = $l->addItem();
+    $i->addParagraph()->addLink($lien->url, $lien->nom);
+    $i->addParagraph()->addFlags('description')->addInline($lien->description);
+  }
+}
+else {
+  $this->document->addParagraph('Aucun lien')
+    ->addFlags('empty');
 }
