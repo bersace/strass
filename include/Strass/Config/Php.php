c67279f2476b51f07c720a98ce207f2761067d92
Étienne BERSAC <bersace03@gmail.com>
2014-02-13 13:45:55 +0100

    Permettre d'ouvrir une configuration inexistante sans warnings

--- include/Strass/Config/Php.php
+++ include/Strass/Config/Php.php
@@ -5,46 +5,50 @@ require_once 'Zend/Registry.php';
 
 class Strass_Config_Php extends Zend_Config
 {
-	protected	$basedir;
-	protected	$name;
-	public	$filename;
-	protected	$config;
+  protected	$basedir;
+  protected	$name;
+  public	$filename;
+  protected	$config;
 
-	public function __construct ($name, $config = null)
-	{
-		$this->name = $name;
-		$this->config = $config;
+  public function __construct ($name, $config = null)
+  {
+    $this->name = $name;
+    $this->config = $config;
 
-		try {
-			$this->basedir = Zend_Registry::get('config_basedir');
-		}
-		catch (Zend_Exception $e) {
-			$this->basedir = 'private/config/';
-		}
+    try {
+      $this->basedir = Zend_Registry::get('config_basedir');
+    }
+    catch (Zend_Exception $e) {
+      $this->basedir = 'private/config/';
+    }
 
-		$this->filename = $this->basedir.$name.'.php';
+    $this->filename = $this->basedir.$name.'.php';
 
-		if ($config === null) {
-			$config = include $this->filename;
-		}
+    if ($config === null) {
+      $config = @include $this->filename;
 
-		parent::__construct ($config, true);
-	}
+      if (!is_array($config)) {
+	$config = array();
+      }
+    }
 
-	public function write()
-	{
-		$dirname = dirname ($this->filename);
-		$dirs = explode ('/', $dirname);
-		$dir = '';
-		foreach ($dirs as $d) {
-			$dir.= $d.'/';
-			if (!file_exists($dir)) {
-				mkdir ($dir);
-			}
-		}
-    
-		file_put_contents ($this->filename,
-				   "<?php\n".
-				   "return ".var_export ($this->toArray(), true).";\n");
-	}
+    parent::__construct ($config, true);
+  }
+
+  public function write()
+  {
+    $dirname = dirname ($this->filename);
+    $dirs = explode ('/', $dirname);
+    $dir = '';
+    foreach ($dirs as $d) {
+      $dir.= $d.DIRECTORY_SEPARATOR;
+      if (!file_exists($dir)) {
+	mkdir ($dir);
+      }
+    }
+
+    file_put_contents($this->filename,
+		      "<?php\n".
+		      "return ".var_export($this->toArray(), true).";\n");
+  }
 }
