cc40e6c4bd2e9f5a8352b98ada715dd4f7f3a2c0
Étienne BERSAC <bersace03@gmail.com>
2014-02-13 23:48:40 +0100

    Rétablissement de l'export vCard d'une fiche d'individu

--- include/Strass/Views/Helpers/CarteIndividu.php
+++ include/Strass/Views/Helpers/CarteIndividu.php
@@ -2,43 +2,43 @@
 
 class Strass_View_Helper_CarteIndividu
 {
-	protected	$view;
+  protected	$view;
 
-	public function setView($view)
-	{
-		$this->view = $view;
-	}
+  public function setView($view)
+  {
+    $this->view = $view;
+  }
 
-	function CarteIndividu($individu)
-	{
-		$v = new vCard();
-		$acl = Zend_Registry::get('acl');
-		$ind = Zend_Registry::get('user');
-		if ($acl->isAllowed($ind, $individu, 'fiche')) {
-			$v->setName($individu->nom, $individu->prenom);
-			$v->setBirthday($individu->naissance);
+  function CarteIndividu($individu)
+  {
+    $v = new vCard;
+    $acl = Zend_Registry::get('acl');
+    $ind = Zend_Registry::get('user');
+    if ($acl->isAllowed($ind, $individu, 'fiche')) {
+      $v->setName($individu->nom, $individu->prenom);
+      if ($individu->naissance)
+	$v->setBirthday($individu->naissance);
 
-			$t0 = explode("\n", $individu->adresse);
-			$t0 = array_pad($t0, 3, '');
-			list($adresse, $ville, $pays) = $t0;
-			if (preg_match("`(\d{5}) (.*)`", $ville, $res))
-				$v->setAddress("", "", $adresse, $res[2], "", $res[1], $pays);
+      $t0 = explode("\n", $individu->adresse);
+      $t0 = array_pad($t0, 3, '');
+      list($adresse, $ville, $pays) = $t0;
+      if (preg_match("`(\d{5}) (.*)`", $ville, $res))
+	$v->setAddress("", "", $adresse, $res[2], "", $res[1], $pays);
 
-			$v->setPhoneNumber($individu->fixe, "HOME");
-			$v->setPhoneNumber($individu->portable, "CELL");
+      $v->setPhoneNumber($individu->fixe, "HOME");
+      $v->setPhoneNumber($individu->portable, "CELL");
 
-			if ($photo = $individu->getImage())
-				$v->setPhoto('jpeg', file_get_contents($photo));
+      if ($photo = $individu->getImage())
+	$v->setPhoto('jpeg', file_get_contents($photo));
 
-			$v->setEmail($individu->adelec);
-			$v->setJabber($individu->jabberid);
-			$v->setURL($this->view->urlIndividu($individu, 'fiche', 'individus', true, true));
-		}
-		else {
-			$v->setName($individu->getName());
-			$v->setBirthday(substr($individu->naissance, 0, 4));
-		}
+      $v->setEmail($individu->adelec);
+      $v->setURL($this->view->urlIndividu($individu, 'fiche', 'individus', true, true));
+    }
+    else {
+      $v->setName($individu->getName());
+      $v->setBirthday(substr($individu->naissance, 0, 4));
+    }
 
-		array_push($this->view->vcards, $v);
-	}
+    array_push($this->view->vcards, $v);
+  }
 }
