425c2fddf5277092552cf8bb3cd46a1434d0ba47
Étienne BERSAC <bersace03@gmail.com>
2014-02-15 13:01:40 +0100

    Utiliser les ACL pour les droits du menu principal

--- include/Strass/Addon/Menu.php
+++ include/Strass/Addon/Menu.php
@@ -6,8 +6,19 @@ class Strass_Addon_Menu extends Strass_Addon
 {
   public function initView($view)
   {
-    $config = new Strass_Config_Php('strass');
-    $view->menu = $config->menu->toArray();
-  }
+    $config = Zend_Registry::get('config');
+    $acl = Zend_Registry::get('acl');
+    $user = Zend_Registry::get('user');
 
+    $m = array();
+    foreach ($config->menu as $item) {
+      if ($item->acl) {
+	list($_, $resource, $action) = $item->acl->toArray();
+	if (!$acl->isAllowed($user, $resource, $action))
+	  continue;
+      }
+      $m[] = $item->toArray();
+    }
+    $view->menu = $m;
+  }
 }
