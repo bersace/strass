beaaee7be706a7b75558e1947f0f36a58902713b
Étienne BERSAC <bersace03@gmail.com>
2014-02-15 15:30:01 +0100

    Ne pas traiter les droits du menu dans la vue

--- include/Strass/Views/Scripts/addons/menu.wtk.php
+++ include/Strass/Views/Scripts/addons/menu.wtk.php
@@ -1,22 +1,12 @@
 <?php
 
-  // menu
 $menu = $this->addons->addSection ('menu', 'Menu');
-$list = $menu->addChild (new Wtk_List ());
-
-// réinitialiser tout les paramètres de la requête pour ne pas avoir
-// de confusion.
-$controller = Zend_Controller_Front::getInstance()->getRequest()->getParam('controller');
-$controller = $controller == 'index' ? null : $controller;
+$list = $menu->addList();
 
 foreach ($this->menu as $item) {
-	if (isset($item['private']) && Zend_Registry::get('user')->username == 'nobody')
-		continue;
-
-	$urlOptions = $item['url'];
-	$link = new Wtk_Link ($this->url($urlOptions, true),
-			      new Wtk_Metas ($item['metas']));
-	$i = $list->addItem($link);
-	$i->addFlags($urlOptions['controller'] ? $urlOptions['controller'] : 'index',
-		     $controller == $urlOptions['controller'] ? 'current' : null);
+  extract($item);
+  $url = array_merge(array('controller' => null, 'action' => null),
+		     $url);
+  $list->addItem()->addFlags($url['controller'], $url['action'])
+    ->addLink($this->url($url, true), new Wtk_Metas($metas));
 }
