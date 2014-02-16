206548858609ff79ac333fbf6fcde231f02c1667
Étienne BERSAC <bersace03@gmail.com>
2014-02-16 14:34:09 +0100

    Renommer association en mouvement

--- include/Strass/Migrate/To22.php
+++ include/Strass/Migrate/To22.php
@@ -4,12 +4,20 @@ class Strass_Migrate_To22 extends Strass_MigrateHandler {
   function online($db)
   {
     $old = new Strass_Config_Php('strass');
+
     $new = new Strass_Config_Php('strass', array());
     $new->metas = $old->site->metas;
+
     $system = $old->site->toArray();
+    $system['mouvement'] = $system['association'];
+
+    unset($system['association']);
     unset($system['metas']);
     unset($system['rubrique']);
+
     $new->system = $system;
     $new->write();
+
+    Zend_Registry::set('config', $new);
   }
 }
