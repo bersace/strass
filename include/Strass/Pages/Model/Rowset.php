4c64fbbb68c532c172931fdd1548e89fbf488bfc
Étienne BERSAC <bersace03@gmail.com>
2014-02-16 23:41:19 +0100

    Se rabattre sur la dernière page si une page inexistante est donnée

--- include/Strass/Pages/Model/Rowset.php
+++ include/Strass/Pages/Model/Rowset.php
@@ -2,87 +2,87 @@
 
 class Strass_Pages_Model_Rowset extends Wtk_Pages_Model
 {
-	protected $rowcount;
+  public $row_count;
 
-	function __construct(Zend_Db_Table_Select $select, $count= 15, $current = 1)
-	{
-	  $table = $select->getTable();
-	  $this->select = $select;
-	  $this->rowcount = $table->countRows($select);
+  function __construct(Zend_Db_Table_Select $select, $count=15, $current=1)
+  {
+    $table = $select->getTable();
+    $this->select = $select->distinct();
+    $this->row_count = $table->countRows($select);
+    $this->pages_count = intval(ceil(($this->row_count / $count)));
+    $current = min($current, $this->pages_count);
 
-	  // selection les tuples de cette pages.
-	  $select->limitPage($current, $count);
-	  $rowset = $table->fetchAll($select);
+    // selection les tuples de cette pages.
+    $select->limitPage($current, $count);
+    $rowset = $table->fetchAll($select);
 
-	  parent::__construct($rowset, $count, $current);
+    parent::__construct($rowset, $count, $current);
 
-	  $this->pages_count = intval(ceil(($this->rowcount / $this->item_per_page)));
-	  $this->pages_id = range(1, $this->pagesCount(), 1);
-	}
+    $this->pages_id = range(1, $this->pagesCount(), 1);
+  }
 
-	function pagesCount()
-	{
-		return $this->pages_count;
-	}
+  function pagesCount()
+  {
+    return $this->pages_count;
+  }
 
-	function getPagesIds()
-	{
-		return $this->pages_id;
-	}
+  function getPagesIds()
+  {
+    return $this->pages_id;
+  }
 
-	function fetch($id = null)
-	{
-		return array_key_exists($id, $this->data) ? $this->data[$id] : null;
-	}
+  function fetch($id = null)
+  {
+    return array_key_exists($id, $this->data) ? $this->data[$id] : null;
+  }
 
-	function getPrevId($ref = null)
-	{
-		return $this->getRelId($ref, -1);
-	}
+  function getPrevId($ref = null)
+  {
+    return $this->getRelId($ref, -1);
+  }
 
-	function getNextId($ref = null)
-	{
-		return $this->getRelId($ref, +1);
-	}
+  function getNextId($ref = null)
+  {
+    return $this->getRelId($ref, +1);
+  }
 
-	// détermine l'identifiant de la page précédente ou suivante par
-	// rapport à $ref;
-	protected function getRelId($ref, $sens)
-	{
-	  $ref = $ref ? $ref : $this->current;
-	  $r = array_flip($this->pages_id);
-	  $i = $r[$ref]+$sens;
-	  return array_key_exists($i, $this->pages_id) ? $this->pages_id[$i] : null;
-	}
+  // détermine l'identifiant de la page précédente ou suivante par
+  // rapport à $ref;
+  protected function getRelId($ref, $sens)
+  {
+    $ref = $ref ? $ref : $this->current;
+    $r = array_flip($this->pages_id);
+    $i = $r[$ref]+$sens;
+    return array_key_exists($i, $this->pages_id) ? $this->pages_id[$i] : null;
+  }
 
-	public function count()
-	{
-	  return $this->data->count();
-	}
+  public function count()
+  {
+    return $this->data->count();
+  }
 
-	public function rewind()
-	{
-	  $this->data->rewind();
-	}
-  
-	public function current()
-	{
-	  return $this->data->current();
-	}
+  public function rewind()
+  {
+    $this->data->rewind();
+  }
 
-	public function key()
-	{
-	  return $this->data->key();
-	}
+  public function current()
+  {
+    return $this->data->current();
+  }
 
-	public function next()
-	{
-	  $this->data->next();
-	}
+  public function key()
+  {
+    return $this->data->key();
+  }
 
-	public function valid()
-	{
-	  return $this->data->valid();
-	}
+  public function next()
+  {
+    $this->data->next();
+  }
+
+  public function valid()
+  {
+    return $this->data->valid();
+  }
 }
-
