cdaab39c90328ee1e160469903788597193e0407
Étienne BERSAC <bersace03@gmail.com>
2014-02-16 14:31:28 +0100

    Suppressions de plugins obsolètes

--- include/Strass/Controller/Action/Helper/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Strass_Controller_Action_Helper_Config extends Zend_Controller_Action_Helper_Abstract
-{
-	public function direct($name)
-	{
-		return new Strass_Config_Php($name);
-	}
-}
