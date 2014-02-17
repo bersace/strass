7e8fce7b0158732380a772404e873d1836559c02
Étienne BERSAC <bersace03@gmail.com>
2014-02-17 17:43:04 +0100

    Masquer totalement le détails des bugs.

--- include/Strass/Views/Scripts/error/error.wtk.php
+++ include/Strass/Views/Scripts/error/error.wtk.php
@@ -1,51 +1,52 @@
 <?php
-// simple listage des erreurs.
-if (count($this->errors > 1)) {
-  $this->document->setTitle("Erreurs");
-}
+
+$dialog = $this->document->addDialog("Bug !")
+  ->setId("errors")->addFlags('error');
+
+$aide = $dialog->addSection('aide');
+$details = $dialog->addSection('details', 'Détails');
 
 foreach ($this->errors as $i => $error) {
-  if ($error instanceof Strass_Controller_Action_Exception_Forbidden)
-    $titre = "Accès refusé";
-  else if ($error instanceof Strass_Controller_Action_Exception_Notice)
-    $titre = "Notice";
-  else
-    $titre = "Erreur".(count($this->errors)>1 ? " #".($i+1) : "");
-
-  $this->document->setTitle(null);
-
-  $section = $this->document->addDialog($titre)
-    ->setId("error".$i)
-    ->addFlags('error');
-
-  $section->addText("// ".$error->getMessage()." // \n");
-
   if ($error instanceof Strass_Controller_Action_Exception_Forbidden) {
+    $dialog->title = $titre = "Accès refusé";
+    $dialog->addFlags('forbidden');
     if (Zend_Registry::get('user')) {
       $config = Zend_Registry::get('config');
-      $section->addText("Si vous devriez avoir accès au site, ".
-			"[mailto:".$config->system->admin." contactez le webmestre].");
+      $aide->addText("Si vous devriez avoir accès au site, ".
+		     "[mailto:".$config->system->admin." contactez le webmestre].");
     }
     else {
-      $section->addParagraph("Si vous êtes inscrit, identifiez-vous. Sinon, ",
+      $aide->addParagraph("Si vous êtes inscrit, identifiez-vous. Sinon, ",
 			     $this->lien(array('controller' => 'membres',
 					       'action' => 'inscription'),
 					 "inscrivez-vous", true),
 			     ".");
     }
   }
+  if ($error instanceof Strass_Controller_Action_Exception_Notice)
+    $dialog->title = $titre = "Attention";
+  else if ($i == 0) {
+    $dialog->title = "Bug !";
+    $titre = null;
+    $dialog->addFlags('bug');
+    $aide->addText("Désolé pour la gêne occasionée. ".
+		   "Le bug est enregistré dans le journal et nous ferons notre possible ".
+		   "pour le corriger. //En attendant, essayez de le contourner !//");
+  }
 
-  $d = $section->addSection(null, 'Détails')->addFlags('details');
-  $d->addText("à {{".$error->getFile().":".$error->getLine()."}}\n");
-  $backtrace = $d->addSection("backtrace".$i, "Backtrace")->addFlags('trace');
+  $section = $details->addSection(null, $titre)->addFlags('error');
+  $section->addText("{{".get_class($error). "}}: // ".$error->getMessage()." // \n");
+
+  $section->addText("à {{".$error->getFile().":".$error->getLine()."}}\n");
+  $backtrace = $section->addSection("backtrace".$i, "Backtrace")->addFlags('trace');
   $list = $backtrace->addList()->setOrdered()->setReversed();
   foreach ($error->getTrace() as $step) {
     extract($step);
-    $list->addItem()->addText((isset($file) ? $file.":".$line." " : "")." ".
-			      (isset($class) ? $class."::" : "").$step['function']);
+    $list->addItem()->addText("{{".(isset($file) ? $file.":".$line." " : "")." ".
+			      (isset($class) ? $class."::" : "").$step['function']."}}");
   }
 
   if (!($error instanceof Zend_Controller_Action_Exception)) {
-    $section->addFlags('showtrace');
+    $dialog->addFlags('showtrace');
   }
 }
