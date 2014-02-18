f77f04118f95aa54729789d0af19070fabfd3fcd
Étienne BERSAC <bersace03@gmail.com>
2014-02-18 06:29:09 +0100

    Ajouter un bouchon de page vide si pas de documents

--- include/Strass/Views/Scripts/documents/index.wtk.php
+++ include/Strass/Views/Scripts/documents/index.wtk.php
@@ -1,9 +1,14 @@
 <?php
 
-$l = $this->document->addList()
-  ->addFlags('vignettes', 'documents');
+if ($this->docs->count()) {
+  $l = $this->document->addList()
+    ->addFlags('vignettes', 'documents');
 
-foreach($this->docs as $doc) {
-  $l->addItem($this->vignetteDocument($doc))
-    ->addFlags('vignette', 'document');
+  foreach($this->docs as $doc) {
+    $l->addItem($this->vignetteDocument($doc))
+      ->addFlags('vignette', 'document');
+  }
+}
+else {
+  $this->document->addParagraph('Aucun document')->addFlags('empty');
 }
