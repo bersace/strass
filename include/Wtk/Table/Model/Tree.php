fa17d5c180d8e82002e538aec8f66b02672e4aaf
Étienne BERSAC <bersace03@gmail.com>
2014-02-18 01:54:40 +0100

    Trier le modèle d'arbre selon le parcours en profondeur

--- include/Wtk/Table/Model/Tree.php
+++ include/Wtk/Table/Model/Tree.php
@@ -1,5 +1,23 @@
 <?php
 
+/* Retourne 1 si $a est le cadet ou le fils de $b, -1 s'il est le père ou l'ainé. */
+function wtk_table_tree_path_cmp($a, $b)
+{
+  $max = max(count($a), count($b));
+
+  for ($i = 0; $i < $max; $i++) {
+    if (!array_key_exists($i, $a))
+      return 1;
+    if (!array_key_exists($i, $b))
+      return -1;
+
+    if ($cmp = $a[$i] - $b[$i])
+      return $cmp;
+  }
+
+  return 0;
+}
+
 /*
   Chaque ligne est référencée par un $$path$$. Un path est une liste
   des index de chaque nœuds parent et de la feuille. Il faut désigner
@@ -37,7 +55,25 @@ class Wtk_Table_Model_Tree extends Wtk_Table_Model
     array_push($path, $child_id);
     $values[0] = $path;
 
-    call_user_func_array(array('parent', 'append'), $values);
+    $row = array();
+    $ids = $this->getColumnIds();
+    foreach ($ids as $i => $col)
+      $row[$col] = array_key_exists($i, $values) ? $values[$i] : null;
+
+    $count = count($this->rows);
+    $patha = $path;
+    $added = false;
+    for($i = 0; $i < $count; $i++) {
+      $pathb = $this->rows[$i]['$$path$$'];
+      if (wtk_table_tree_path_cmp($patha, $pathb) > 0) {
+	  array_splice($this->rows, $i, 0, array($row));
+	  $added = true;
+	  break;
+      }
+    }
+    if (!$added)
+      array_push($this->rows, $row);
+
     return $path;
   }
 }
