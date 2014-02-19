058525930155cab1268ed87607303207da637aca
Étienne BERSAC <bersace03@gmail.com>
2014-02-20 00:55:22 +0100

    Lancer une exception si pas de méthode render*

--- include/Wtk/Pages/Renderer/Form.php
+++ include/Wtk/Pages/Renderer/Form.php
@@ -40,6 +40,8 @@ class Wtk_Pages_Renderer_Form extends Wtk_Pages_Renderer
   function render($id, $data, $container)
   {
     $method = 'render'.ucfirst($id);
+    if (!method_exists($this, $method))
+      throw new Exception("Missing render method $method");
     call_user_func(array($this, $method), $data, $container);
   }
 
