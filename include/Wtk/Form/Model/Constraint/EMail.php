14057456bbf219165a85f8fb6f55c2cb003a0aa5
Étienne BERSAC <bersace03@gmail.com>
2014-02-19 17:32:50 +0100

    Permettre les adresses @localhost

--- include/Wtk/Form/Model/Constraint/EMail.php
+++ include/Wtk/Form/Model/Constraint/EMail.php
@@ -4,6 +4,6 @@ class Wtk_Form_Model_Constraint_EMail extends Wtk_Form_Model_Constraint_Match
 {
   function __construct($instance) {
     parent::__construct($instance,
-			'/^[[:alnum:]\._+-]{3,}@[[:alnum:]\._-]{3,}\.[[:alnum:]]{2,6}$/');
+			'/^[[:alnum:]\._+-]{3,}@[[:alnum:]\._-]{3,}(\.[[:alnum:]]{2,6})*$/');
   }
 }
