0dbbd227cc05507feb134c87f2d09cde1ffbff85
Étienne BERSAC <bersace03@gmail.com>
2014-03-05 22:03:36 +0100

    Retourner une erreur 403 pour les accès interdits, plutôt que 500

--- include/Strass/Controller/Action/Exception/Forbidden.php
+++ include/Strass/Controller/Action/Exception/Forbidden.php
@@ -4,7 +4,6 @@ class Strass_Controller_Action_Exception_Forbidden extends Strass_Controller_Act
 {
   function __construct($message=null, $code=403)
   {
-    $args = func_get_args();
-    call_user_func_array(array('parent', '__construct'), $args);
+    parent::__construct($message, $code);
   }
 }
