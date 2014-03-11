0d0750ff762e92adfd707d585cccd7d833cb2eb0
Étienne BERSAC <bersace03@gmail.com>
2014-03-11 21:47:47 +0100

    Générer un lien propre vers les citations
    Le bug classique est lorsqu'on est sur une adresse avec un paramètre page,
    le lien héritait du paramètre. Résultat étrange avec page/legal ;-)

--- include/Strass/Views/Scripts/addons/citation.wtk.php
+++ include/Strass/Views/Scripts/addons/citation.wtk.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($this->citation) {
-  $link = $this->lien(array('controller' => 'citation', 'action' => 'index'), 'Citation');
+  $link = $this->lien(array('controller' => 'citation', 'action' => 'index'), 'Citation', true);
   $s = $this->addons->addSection('citation', $link);
   $s->addParagraph("« ".$this->citation->texte." »")->addFlags('citation');
   $s->addParagraph($this->citation->auteur)->addFlags('signature');
