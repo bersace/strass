e5c2a3bc002fe929a09ff61bad32ee37d138e66e
Étienne BERSAC <bersace03@gmail.com>
2014-03-11 14:42:28 +0100

    Lister les autres unités racines dans le menu

--- include/Strass/Addon/Menu.php
+++ include/Strass/Addon/Menu.php
@@ -4,13 +4,13 @@ require_once 'Strass/Addon.php';
 
 class Strass_Addon_Menu extends Strass_Addon_Liens
 {
-  static $menu = array (array('metas' => array('label' => 'Accueil'),
+  static $menu = array (array('metas' => 'Accueil',
 			      'url'   => array()),
-			array('metas' => array('label' => "Livre d'or"),
+			array('metas' => "Livre d'or",
 			      'url'   => array('controller' => 'livredor')),
-			array('metas' => array('label' => 'Liens'),
+			array('metas' => 'Liens',
 			      'url'   => array('controller' => 'liens')),
-			array('metas' => array('label' => 'Administration'),
+			array('metas' => 'Administration',
 			      'url'   => array('controller' => 'admin'),
 			      'acl'   => array(null, 'site', 'admin')),
 			);
@@ -25,6 +25,15 @@ class Strass_Addon_Menu extends Strass_Addon_Liens
     foreach (self::$menu as $item) {
       $acl = array_key_exists('acl', $item) ? $item['acl'] : array();
       $this->append($item['metas'], $item['url'], $acl, true);
+    }
+
+    $t = new Unites;
+    $racines = $t->findRacines();
+    foreach($racines as $i => $u) {
+      if (!$i)
+	continue; // On saute l'unité racine par défaut
+
+      $this->append($u->getName(), array('unite' => $u->slug), array(), true);
     }
 
     return parent::initView($view);
