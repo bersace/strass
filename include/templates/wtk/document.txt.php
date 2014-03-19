b525cb479c9407f4b88e1f192a4512f31de61753
Étienne BERSAC <bersace03@gmail.com>
2014-03-19 13:06:14 +0100

    Rétablir la génération de document texte

--- include/templates/wtk/document.txt.php
+++ include/templates/wtk/document.txt.php
@@ -3,12 +3,5 @@ $creation = $metas->creation;
 $year = date('Y');
 
 wtk_txt_data($this->content, '', 72);
-wtk_txt_data($this->footer, '', 72);
 ?>
 <?php $this->content->output(); ?>
-
-<?php
-if ($footer = trim($this->footer->render()))
-echo wtk_txt_center(wtk_txt_pad('', 36, '—'), 72)."\n".$footer;
-
-
