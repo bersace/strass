fa20e1ccfb817c67cd22dd72a04b41de6b703e29
Étienne BERSAC <bersace03@gmail.com>
2014-03-19 12:30:58 +0100

    Grouper l'aperçu d'un article dans un div

--- include/Strass/Views/PagesRenderer/Articles.php
+++ include/Strass/Views/PagesRenderer/Articles.php
@@ -20,11 +20,13 @@ class Strass_Views_PagesRenderer_Articles extends Wtk_Pages_Renderer
     $s->addFlags('article');
     $s->addChild($this->view->vignetteIndividu($article->findAuteur()));
 
-    $t = $s->addText(wtk_first_lines($article->getBoulet() . "\n\n". $article->article));
+    $contenu = $s->addSection()->addFlags('contenu');
+
+    $t = $contenu->addText(wtk_first_lines($article->getBoulet() . "\n\n". $article->article));
     $tw = $t->getTextWiki();
     $tw->setRenderConf('Xhtml', 'image', 'base', $article->getDossier());
-    $s->addParagraph($this->view->signature($article), ".")->addFlags('signature');
+    $contenu->addParagraph($this->view->signature($article), ".")->addFlags('signature');
     $lien = $this->view->lienArticle($article, 'Lire la suite…');
-    $s->addParagraph($lien)->addFlags('suite');
+    $contenu->addParagraph($lien)->addFlags('suite');
   }
 }
