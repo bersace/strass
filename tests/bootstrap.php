4badb23357290721e8b0d30de807355fedf2ce40
Étienne BERSAC <bersace03@gmail.com>
2014-03-27 17:46:43 +0100

    Rétablir l'exécution des tests unitaires

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,23 +3,17 @@
 $_ENV['STRASS_ROOT'] = 'tests/root/';
 $_ENV['STRASS_UNIT_TEST']  = '1';
 
-umask(0022);
-date_default_timezone_set('Europe/Paris');
-setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8', 'fr-FR', 'fra');
-
 $paths = explode(':', get_include_path());
 array_shift($paths);
 array_unshift($paths, '.', dirname(__FILE__) . '/../include');
 set_include_path(implode(':',$paths));
 
+require_once('Strass.php');
 require_once('Orror.php');
-require_once('Zend/Loader/Autoloader.php');
-$loader = Zend_Loader_Autoloader::getInstance();
-$loader->registerNamespace('Dio_');
-$loader->registerNamespace('Wtk_');
-$loader->registerNamespace('Strass_');
 
+Strass::bootstrap();
 Orror::init(E_ALL | E_STRICT);
+
 Strass_Cache::setup();
 Strass_Db::setup();
 Zend_Registry::set('config', new Zend_Config(array(), true));
