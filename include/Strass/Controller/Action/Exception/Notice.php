271884fc381653388c4db214335648b95ee6ec28
Étienne BERSAC <bersace03@gmail.com>
2014-09-22 16:30:04 +0200

    Rétablir l'affichage de l'aide des erreurs Notice

--- include/Strass/Controller/Action/Exception/Notice.php
+++ include/Strass/Controller/Action/Exception/Notice.php
@@ -2,8 +2,8 @@
 
 class Strass_Controller_Action_Exception_Notice extends Strass_Controller_Action_Exception
 {
-  function __construct($message=null, $code=200)
+  function __construct($message=null, $code=200, $aide=null)
   {
-    parent::__construct($message, $code);
+    parent::__construct($message, $code, $aide);
   }
 }
