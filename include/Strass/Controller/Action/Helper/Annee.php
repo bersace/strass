2ce7df714c12af016d38ef4e1a65c705fb89c2f9
Étienne BERSAC <bersace03@gmail.com>
2014-09-28 17:23:35 +0200

    Assouplir les date de début et de fin d'année

--- include/Strass/Controller/Action/Helper/Annee.php
+++ include/Strass/Controller/Action/Helper/Annee.php
@@ -2,34 +2,36 @@
 
 class Strass_Controller_Action_Helper_Annee extends Zend_Controller_Action_Helper_Abstract
 {
-	function direct($fallback = true)
-	{
-		$annee = $this->getRequest()->getParam('annee');
-		$annee = $annee ? $annee : ($fallback ? $this->cetteAnnee() : null);
-		return intval($annee);
-	}
+  function direct($fallback = true)
+  {
+    $annee = $this->getRequest()->getParam('annee');
+    $annee = $annee ? $annee : ($fallback ? $this->cetteAnnee() : null);
+    return intval($annee);
+  }
 
-	function setBranche($annee)
-	{
-	  if (!$annee)
-	    return;
+  function setBranche($annee)
+  {
+    if (!$annee)
+      return;
 
-	  $this->_actionController->branche->append($annee, array('annee' => $annee));
-	  return $annee;
-	}
+    $this->_actionController->branche->append($annee, array('annee' => $annee));
+    return $annee;
+  }
 
-	static function dateDebut($annee)
-	{
-		return $annee.'-09-01';
-	}
+  static function dateDebut($annee)
+  {
+    /* on commence parfois fin août */
+    return $annee.'-08-30';
+  }
 
-	static function dateFin($annee)
-	{
-		return ($annee+1).'-08-31';
-	}
+  static function dateFin($annee)
+  {
+    /* on termine parfois début septembre */
+    return ($annee+1).'-09-03';
+  }
 
-	static function cetteAnnee()
-	{
-		return strftime('%Y', time()-8*30*24*60*60);
-	}
+  static function cetteAnnee()
+  {
+    return strftime('%Y', time()-8*30*24*60*60);
+  }
 }
