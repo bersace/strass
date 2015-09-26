74a9886cbda427658b945f47bbd54cad63b1d84c
Étienne BERSAC <bersace03@gmail.com>
2015-09-26 14:06:07 +0200

    Réidenter S/A/Menu.php

--- include/Strass/Addon/Menu.php
+++ include/Strass/Addon/Menu.php
@@ -4,39 +4,44 @@ require_once 'Strass/Addon.php';
 
 class Strass_Addon_Menu extends Strass_Addon_Liens
 {
-  static $menu = array (array('metas' => 'Accueil',
-			      'url'   => array()),
-			array('metas' => "Livre d'or",
-			      'url'   => array('controller' => 'livredor')),
-			array('metas' => 'Liens',
-			      'url'   => array('controller' => 'liens')),
-			array('metas' => 'Administration',
-			      'url'   => array('controller' => 'admin'),
-			      'acl'   => array(null, 'site', 'admin')),
-			);
+    static $menu = array(
+        array(
+            'metas' => 'Accueil',
+            'url'   => array()),
+        array(
+            'metas' => "Livre d'or",
+            'url'   => array('controller' => 'livredor')),
+        array(
+            'metas' => 'Liens',
+            'url'   => array('controller' => 'liens')),
+        array(
+            'metas' => 'Administration',
+            'url'   => array('controller' => 'admin'),
+            'acl'   => array(null, 'site', 'admin')),
+    );
 
-  function __construct()
-  {
-    parent::__construct('menu', 'Navigation');
-  }
-
-  public function initView($view)
-  {
-    foreach (self::$menu as $item) {
-      $acl = array_key_exists('acl', $item) ? $item['acl'] : array();
-      $this->append($item['metas'], $item['url'], $acl, true);
+    function __construct()
+    {
+        parent::__construct('menu', 'Navigation');
     }
 
-    $t = new Unites;
-    $racines = $t->findRacines();
-    foreach($racines as $i => $u) {
-      if (!$i)
-	continue; // On saute l'unité racine par défaut
+    public function initView($view)
+    {
+        foreach (self::$menu as $item) {
+            $acl = array_key_exists('acl', $item) ? $item['acl'] : array();
+            $this->append($item['metas'], $item['url'], $acl, true);
+        }
 
-      $this->append($u->getFullName(), array('unite' => $u->slug), array(), true);
+        $t = new Unites;
+        $racines = $t->findRacines();
+        foreach($racines as $i => $u) {
+            if (!$i)
+                continue; // On saute l'unité racine par défaut
+
+            $this->append($u->getFullName(), array('unite' => $u->slug), array(), true);
+        }
+
+        parent::initView($view);
+        $view->parent = $view->document->footer->current();
     }
-
-    parent::initView($view);
-    $view->parent = $view->document->footer->current();
-  }
 }
