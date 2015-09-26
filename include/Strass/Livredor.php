5d45ff57a40f5796da20aaa12ee2904db7129cdd
Étienne BERSAC <bersace03@gmail.com>
2015-09-27 01:59:44 +0200

    Réindeter Livredor.php

--- include/Strass/Livredor.php
+++ include/Strass/Livredor.php
@@ -2,29 +2,29 @@
 
 class Livredor extends Strass_Db_Table_Abstract implements Zend_Acl_Resource_Interface
 {
-  protected	$_name			= 'livredor';
-  protected	$_rowClass		= 'Message';
+    protected	$_name			= 'livredor';
+    protected	$_rowClass		= 'Message';
 
-  function initAclResource($acl)
-  {
-    $acl->add($this);
-  }
+    function initAclResource($acl)
+    {
+        $acl->add($this);
+    }
 
-  function getResourceId()
-  {
-    return 'livredor';
-  }
+    function getResourceId()
+    {
+        return 'livredor';
+    }
 }
 
 class Message extends Zend_Db_Table_Row_Abstract implements Zend_Acl_Resource_Interface
 {
-  function initAclResource($acl)
-  {
-    $acl->add($this);
-  }
+    function initAclResource($acl)
+    {
+        $acl->add($this);
+    }
 
-  function getResourceId()
-  {
-    return 'livredor-message-'.$this->id;
-  }
+    function getResourceId()
+    {
+        return 'livredor-message-'.$this->id;
+    }
 }
