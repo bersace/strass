8e60b3ccd69c4b5633aac76f90141f2e0eb73aca
Étienne BERSAC <bersace03@gmail.com>
2015-09-26 14:38:55 +0200

    Réindenter LienIndividu.php

--- include/Strass/Views/Helpers/LienIndividu.php
+++ include/Strass/Views/Helpers/LienIndividu.php
@@ -2,37 +2,36 @@
 
 class Strass_View_Helper_LienIndividu
 {
-  protected	$view;
+    protected	$view;
 
-  public function setView($view)
-  {
-    $this->view = $view;
-  }
-
-  public function lienIndividu($individu,
-			       $label = null,
-			       $action = 'fiche',
-			       $controller = 'individus')
-  {
-    if (!$individu) {
-      return null;
+    public function setView($view)
+    {
+        $this->view = $view;
     }
 
-    $acl = Zend_Registry::get('acl');
-    if ($acl->isAllowed(null, $individu, $action)) {
-      $lien =  $this->view->lien(array('action' => $action,
-				       'individu' => $individu->slug,
-				       'controller' => $controller),
-				 $label ? $label : wtk_nbsp($individu->getFullName()),
-				 true);
+    public function lienIndividu(
+        $individu, $label = null, $action = 'fiche',
+        $controller = 'individus')
+    {
+        if (!$individu) {
+            return null;
+        }
 
-      $etape = $individu->findParentEtapes();
-      if ($etape)
-	$lien->addFlags($etape->slug);
+        $acl = Zend_Registry::get('acl');
+        if ($acl->isAllowed(null, $individu, $action)) {
+            $lien =  $this->view->lien(array('action' => $action,
+            'individu' => $individu->slug,
+            'controller' => $controller),
+            $label ? $label : wtk_nbsp($individu->getFullName()),
+            true);
 
-      return $lien;
+            $etape = $individu->findParentEtapes();
+            if ($etape)
+                $lien->addFlags($etape->slug);
+
+            return $lien;
+        }
+        else
+            return new Wtk_RawText($label ? $label : $individu->getFullname());
     }
-    else
-      return new Wtk_RawText($label ? $label : $individu->getFullname());
-  }
 }
