d4fe372b7dd3cbdb1cffc3f91e5b6a2929d37137
Étienne BERSAC <bersace03@gmail.com>
2015-09-27 01:22:29 +0200

    Réindenter log.wtk.php

--- include/Strass/Views/Scripts/admin/log.wtk.php
+++ include/Strass/Views/Scripts/admin/log.wtk.php
@@ -2,56 +2,57 @@
 
 class Strass_Pages_Renderer_Log extends Wtk_Pages_Renderer
 {
-  private $view;
+    private $view;
 
-  function __construct($view)
-  {
-    $this->view = $view;
-    parent::__construct($view->url(array('page' => '%i')), true,
-			array('previous' => "Précédents",
-			      'next'	 => "Suivants"));
-  }
-
-  function renderContainer()
-  {
-    $model = $m = new Wtk_Table_Model('date', 'level', 'logger', 'label', 'url',
-				      'prenom-nom', 'fiche', 'detail-url');
-    $table = $t = new Wtk_Table($model, true, 'level');
-    $t->addFlags('logs');
-
-    $t->addNewColumn("Niveau", new Wtk_Table_CellRenderer_Text('text', 'level'));
-    $t->addNewColumn("Date", new Wtk_Table_CellRenderer_Link('href', 'detail-url',
-							     'label', 'date'));
-    $t->addNewColumn("Émetteur", new Wtk_Table_CellRenderer_Text('text', 'logger'));
-    $t->addNewColumn("Utilistateur", new Wtk_Table_CellRenderer_Link('href', 'fiche',
-								     'label', 'prenom-nom'));
-    $t->addNewColumn("Message", new Wtk_Table_CellRenderer_Link('href', 'url',
-							     'label', 'label'));
-
-    return $table;
-  }
-
-  function render($id, $event, $table)
-  {
-    $m = $table->getModel();
-    $u = $event->findParentUsers();
-    if ($u) {
-      $i = $u->findParentIndividus();
-      $pn = $i->getFullname(false, false);
-      $fiche = $this->view->url(array('controller' => 'individus', 'action' => 'fiche',
-				      'individu' => $i->slug), true);
+    function __construct($view)
+    {
+        $this->view = $view;
+        parent::__construct($view->url(array('page' => '%i')), true,
+        array('previous' => "Précédents",
+        'next'	 => "Suivants"));
     }
-    else {
-      $pn = "Visiteur";
-      $fiche = null;
+
+    function renderContainer()
+    {
+        $model = $m = new Wtk_Table_Model('date', 'level', 'logger', 'label', 'url',
+        'prenom-nom', 'fiche', 'detail-url');
+        $table = $t = new Wtk_Table($model, true, 'level');
+        $t->addFlags('logs');
+
+        $t->addNewColumn("Niveau", new Wtk_Table_CellRenderer_Text('text', 'level'));
+        $t->addNewColumn("Date", new Wtk_Table_CellRenderer_Link('href', 'detail-url',
+        'label', 'date'));
+        $t->addNewColumn("Émetteur", new Wtk_Table_CellRenderer_Text('text', 'logger'));
+        $t->addNewColumn("Utilistateur", new Wtk_Table_CellRenderer_Link('href', 'fiche',
+        'label', 'prenom-nom'));
+        $t->addNewColumn("Message", new Wtk_Table_CellRenderer_Link('href', 'url',
+        'label', 'label'));
+
+        return $table;
     }
-    $detail_url = $this->view->url(array('controller' => 'admin', 'action' => 'event',
-				      'id' => $event->id), true);
-    $m->append($event->date, strtolower($event->level), $event->logger,
-	       wtk_first_words($event->message, 42), $event->url,
-	       $pn, $fiche, $detail_url);
-  }
+
+    function render($id, $event, $table)
+    {
+        $m = $table->getModel();
+        $u = $event->findParentUsers();
+        if ($u) {
+            $i = $u->findParentIndividus();
+            $pn = $i->getFullname(false, false);
+            $fiche = $this->view->url(array('controller' => 'individus', 'action' => 'fiche',
+            'individu' => $i->slug), true);
+        }
+        else {
+            $pn = "Visiteur";
+            $fiche = null;
+        }
+        $detail_url = $this->view->url(array('controller' => 'admin', 'action' => 'event',
+        'id' => $event->id), true);
+        $m->append($event->date, strtolower($event->level), $event->logger,
+        wtk_first_words($event->message, 42), $event->url,
+        $pn, $fiche, $detail_url);
+    }
 }
 
-$this->document->addPages(null, $this->events,
-			  new Strass_Pages_Renderer_Log($this));
+$this->document->addPages(
+    null, $this->events,
+    new Strass_Pages_Renderer_Log($this));
