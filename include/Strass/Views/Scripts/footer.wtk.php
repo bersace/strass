3c172dad97dcc25bf5c2a64308a42f4113f91717
Étienne BERSAC <bersace03@gmail.com>
2015-10-08 22:37:02 +0200

    Réindenter S/V/S/footer.wtk.php

--- include/Strass/Views/Scripts/footer.wtk.php
+++ include/Strass/Views/Scripts/footer.wtk.php
@@ -4,16 +4,17 @@ $s = $this->document->getFooter()->current()->addSection('about', "À propos");
 
 $created = $this->document->metas->get('DC.Date.created');
 $l = $s->addList();
-$l->addItem('© '.$created.($created == date('Y') ? '' : '-'.date('Y'))." ".
-	    $this->document->metas->get('DC.Creator'))->addFlags('copyright');
-
+$l->addItem(
+    '© '.$created.($created == date('Y') ? '' : '-'.date('Y'))." ".
+    $this->document->metas->get('DC.Creator'))->addFlags('copyright');
 
 $l->addItem()->addFlags('legal')
-->addLink($this->url(array('controller' => 'statiques', 'action' => 'index', 'page' => 'legal'), true),
-          'Mentions légales');
+  ->addLink(
+      $this->url(array('controller' => 'statiques', 'action' => 'index', 'page' => 'legal'), true),
+      'Mentions légales');
 
 $l->addItem($this->page->metas->get('DC.Title'))->addFlags('title');
 $l->addItem($this->page->metas->get('DC.Creator'))->addFlags('author');
 $l->addItem(strftime('%x', strtotime($this->page->metas->get('DC.Date.available'))))->addFlags('date');
 $l->addItem()->addFlags('strass')
-->addLink('https://gitlab.com/strass', 'Propulsé par Strass');
+  ->addLink('https://gitlab.com/strass', 'Propulsé par Strass');
