c6e2426833b7ba79f374dc5d45cac2fb4ff23e95
Étienne BERSAC <bersace03@gmail.com>
2015-10-22 01:19:53 +0200

    Tester la validation du mot de passe admin

--- tests/func/test_groupe_suf/test_00_install.py
+++ tests/func/test_groupe_suf/test_00_install.py
@@ -7,24 +7,27 @@ from fixtures import ADMIN_EMAIL, ADMIN_PASSWORD
 class TestInstaller(ClientTestCase):
     def test_00_missing_association(self):
         (
-            self.client
-            .get()
+            self.client.get()
             .click('button[type=submit]')
-
-            # S'assurer que le champ association est refusé.
-            .find('#control-installation-site-association.invalid')
         )
 
-    def test_01_association(self):
+        # S'assurer que le champ association est refusé.
+        self.assertElementFound(
+            '#control-installation-site-association.invalid')
+
+    def test_01_association_suf(self):
         (
             self.client
-            .get()
-
-            # Étape association
             .click('input#installation-site-association-suf')
             .click('button[type=submit]')
+        )
 
-            # Étape administrateur
+        # On est bien passé à la page administrateur
+        self.assertElementFound('#installation-admin-prenom')
+
+    def test_02_admin_mauvaise_confirmation(self):
+        (
+            self.client
             .fill('input#installation-admin-prenom', 'Isidore')
             .fill('input#installation-admin-nom', 'Installeur')
             .click('input#installation-admin-sexe-h')
@@ -33,11 +36,28 @@ class TestInstaller(ClientTestCase):
                 datetime.date(1990, 10, 21))
             .fill('input#installation-admin-adelec', ADMIN_EMAIL)
             .fill('input#installation-admin-motdepasse', ADMIN_PASSWORD)
+            .fill('input#installation-admin-confirmation',
+                  ADMIN_PASSWORD + 'POUET')
+            .click('button[type=submit].primary')
+        )
+
+        # S'assurer que le champ confirmation est refusé.
+        self.assertElementFound(
+            '#control-installation-admin-confirmation.invalid')
+
+    def test_03_admin_succes(self):
+        (
+            self.client
+            .fill('input#installation-admin-motdepasse', ADMIN_PASSWORD)
             .fill('input#installation-admin-confirmation', ADMIN_PASSWORD)
             .click('button[type=submit].primary')
         )
 
+    def test_99_succes(self):
+        self.client.get()
+
         # L'installation est terminée.
         #
         # Page d'erreur qu'il n'y a pas d'unité :-)
+        self.assertElementFound('#document.http-404')
         self.assertIn("Pas d'unit", self.client.page_source)
