32f260fe56c5aa6bd329ad9c9f9e4afe8d589076
Étienne BERSAC <bersace03@gmail.com>
2015-10-24 17:25:02 +0200

    Réindenter S/V/S/a/liens.wtk.php

--- include/Strass/Views/Scripts/addons/liens.wtk.php
+++ include/Strass/Views/Scripts/addons/liens.wtk.php
@@ -1,15 +1,15 @@
 <?php
 
 if (count($this->liens)) {
-  $connexes = $this->parent->addSection($this->id, $this->titre);
-  $list = $connexes->addList();
-  $current_uri = Zend_Controller_Front::getInstance()->getRequest()->REQUEST_URI;
-  foreach ($this->liens as $lien) {
-    $url = $this->url($lien['urlOptions'], $lien['reset']);
-    $i = $list->addItem();
-    $i->addLink($url, new Wtk_Metas($lien['metas']));
-    $i->addFlags($lien['urlOptions'], split('/', $url), $lien['flags']);
-    if ($current_uri == $url)
-      $i->addFlags('current');
-  }
+    $connexes = $this->parent->addSection($this->id, $this->titre);
+    $list = $connexes->addList();
+    $current_uri = Zend_Controller_Front::getInstance()->getRequest()->REQUEST_URI;
+    foreach ($this->liens as $lien) {
+        $url = $this->url($lien['urlOptions'], $lien['reset']);
+        $i = $list->addItem();
+        $i->addLink($url, new Wtk_Metas($lien['metas']));
+        $i->addFlags($lien['urlOptions'], split('/', $url), $lien['flags']);
+        if ($current_uri == $url)
+            $i->addFlags('current');
+    }
 }
