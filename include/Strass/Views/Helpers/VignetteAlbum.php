953dfd6c4857189c314444ad68cce175bf719e8d
Étienne BERSAC <bersace03@gmail.com>
2015-10-24 18:46:05 +0200

    Réindenter S/V/H/VignetteAlbum.php

--- include/Strass/Views/Helpers/VignetteAlbum.php
+++ include/Strass/Views/Helpers/VignetteAlbum.php
@@ -2,42 +2,45 @@
 
 class Strass_View_Helper_VignetteAlbum
 {
-  protected	$view;
+    protected	$view;
 
-  public function setView($view)
-  {
-    $this->view = $view;
-  }
-
-  public function vignetteAlbum($album,
-				$label = null,
-				$urlOptions = array())
-  {
-    if (!$album)
-      return;
-
-    $this->view->document->addStyleComponents('vignette');
-
-    $urlOptions = array_merge(array('controller' => 'photos',
-				    'action' => 'consulter',
-				    'album' => $album->slug),
-			      $urlOptions);
-    $photo = $album->getPhotoAleatoire();
-    $label = $label ? $label : $album->getIntituleCourt();
-    $item = new Wtk_Container;
-    $w = $item->addSection()
-      ->addFlags('wrapper');
-    $item->addParagraph($label)->addFlags('label');
-    $link = new Wtk_Link($this->view->url($urlOptions, true, true), $label, $item);
-    $link->addFlags('vignette', 'album');
-
-    if ($photo)
-      $w->addImage($photo->getCheminVignette(), $photo->titre, $album->getIntituleComplet());
-    else {
-      $w->addParagraph("Pas d'image !")->addFlags('empty', 'image');
-      $link->addFlags('empty');
+    public function setView($view)
+    {
+        $this->view = $view;
     }
 
-    return $link;
-  }
+    public function vignetteAlbum($album, $label=null, $urlOptions=array())
+    {
+        if (!$album)
+            return;
+
+        $this->view->document->addStyleComponents('vignette');
+
+        $urlOptions = array_merge(
+            array(
+                'controller' => 'photos',
+                'action' => 'consulter',
+                'album' => $album->slug),
+            $urlOptions);
+        $photo = $album->getPhotoAleatoire();
+        $label = $label ? $label : $album->getIntituleCourt();
+        $item = new Wtk_Container;
+        $w = $item->addSection()
+                  ->addFlags('wrapper');
+        $item->addParagraph($label)->addFlags('label');
+        $link = new Wtk_Link(
+            $this->view->url($urlOptions, true, true), $label, $item);
+        $link->addFlags('vignette', 'album');
+
+        if ($photo)
+            $w->addImage(
+                $photo->getCheminVignette(),
+                $photo->titre, $album->getIntituleComplet());
+        else {
+            $w->addParagraph("Pas d'image !")->addFlags('empty', 'image');
+            $link->addFlags('empty');
+        }
+
+        return $link;
+    }
 }
