a766c694e97ab7d9fc524b827f7e13b3c8a96d28
Étienne BERSAC <bersace03@gmail.com>
2015-10-24 19:05:21 +0200

    Réindenter S/V/H/VignettePhoto.php

--- include/Strass/Views/Helpers/VignettePhoto.php
+++ include/Strass/Views/Helpers/VignettePhoto.php
@@ -2,36 +2,40 @@
 
 class Strass_View_Helper_VignettePhoto
 {
-  protected	$view;
+    protected   $view;
 
-  public function setView($view)
-  {
-    $this->view = $view;
-  }
+    public function setView($view)
+    {
+        $this->view = $view;
+    }
 
-  public function vignettePhoto($photo, $label = null,$urlOptions = array())
-  {
-    if (!$photo)
-      return;
+    public function vignettePhoto($photo, $label = null,$urlOptions = array())
+    {
+        if (!$photo)
+            return;
 
-    $urlOptions = array_merge(array('controller'	=> 'photos',
-				    'action'		=> 'voir',
-				    'photo'		=> $photo->slug),
-			      $urlOptions);
+        $urlOptions = array_merge(
+            array(
+                'controller'    => 'photos',
+                'action'        => 'voir',
+                'photo'         => $photo->slug),
+            $urlOptions);
 
-    $this->view->document->addStyleComponents('vignette');
-    $label = $label ? $label : $photo;
-    $page = Zend_Registry::get('page');
-    $item = new Wtk_Container;
-    $item->addSection()
-      ->addFlags('wrapper')
-      ->addImage($photo->getCheminVignette(),
-		 $photo->titre.' '.$page->metas->get('DC.Subject'),
-		 $photo->titre);
-    $item->addParagraph($label)->addFlags('label');
-    $link = new Wtk_Link($this->view->url($urlOptions, true, true).'#document',
-			 $label, $item);
-    $link->addFlags('vignette', 'photo');
-    return $link;
-  }
+        $this->view->document->addStyleComponents('vignette');
+        $label = $label ? $label : $photo;
+        $page = Zend_Registry::get('page');
+        $item = new Wtk_Container;
+        $item->addSection()
+             ->addFlags('wrapper')
+             ->addImage(
+                 $photo->getCheminVignette(),
+                 $photo->titre.' '.$page->metas->get('DC.Subject'),
+                 $photo->titre);
+        $item->addParagraph($label)->addFlags('label');
+        $link = new Wtk_Link(
+            $this->view->url($urlOptions, true, true).'#document',
+            $label, $item);
+        $link->addFlags('vignette', 'photo');
+        return $link;
+    }
 }
