ea6ab61c384fba7748a2ed415be44eda1a203feb
Étienne BERSAC <bersace03@gmail.com>
2015-10-25 10:51:39 +0100

    Réindenter S/Version.php

--- include/Strass/Version.php
+++ include/Strass/Version.php
@@ -1,26 +1,26 @@
 <?php
 
 final class Strass_Version {
-  const PROJET = '3.0dev';
-  const DATA = 20;
+    const PROJET = '3.0dev';
+    const DATA = 20;
 
-  static $version_filename = 'private/STRASS_VERSION';
+    static $version_filename = 'private/STRASS_VERSION';
 
-  static function dataCurrent() {
-    if (file_exists(self::$version_filename)) {
-      return intval(trim(@file_get_contents(self::$version_filename)));
+    static function dataCurrent() {
+        if (file_exists(self::$version_filename)) {
+            return intval(trim(@file_get_contents(self::$version_filename)));
+        }
+        else if (file_exists('config/knema/db.php')) {
+            /* Installation non versionnée (morel et suf1520) */
+            return 1;
+        }
+        else {
+            /* In principio erat version zero. Rien n'est installé */
+            return 0;
+        }
     }
-    else if (file_exists('config/knema/db.php')) {
-      /* Installation non versionnée (morel et suf1520) */
-      return 1;
-    }
-    else {
-      /* In principio erat version zero. Rien n'est installé */
-      return 0;
-    }
-  }
 
-  static function save($version) {
-    file_put_contents(self::$version_filename, (string) $version);
-  }
+    static function save($version) {
+        file_put_contents(self::$version_filename, (string) $version);
+    }
 }
