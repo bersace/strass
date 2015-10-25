6c5ea5f94790ed6974828ab8fa682a014a07ae21
Étienne BERSAC <bersace03@gmail.com>
2015-10-25 11:37:05 +0100

    Revue du routage de dév
    C'est beaucoup plus rapide ainsi, quand on a lu la doc. Les statiques
    sont gérée une fois, par le serveur de dév. Les URL en UTF-8 sont
    décodée. Le code est dédupliqué. Les logs sont moins verbeux.

--- devel.php
+++ devel.php
@@ -1,35 +1,51 @@
 <?php
+/* Mini serveur de static pour le développement. */
 
-function content_type($uri) {
-  $path = pathinfo($_SERVER['REQUEST_URI']);
-  switch($path['extension']) {
-  case 'css':
-    return 'text/css';
-  case 'png':
-    return 'image/png';
-  case 'jpeg';
-  case 'jpg':
-    return 'image/jpeg';
-  default:
-    return 'text/plain';
-  }
+function content_type($extension) {
+    switch($extension) {
+    case 'ttf':
+        return 'application/x-font-ttf';
+    case 'css':
+        return 'text/css';
+    case 'png':
+        return 'image/png';
+    case 'jpeg';
+    case 'jpg':
+        return 'image/jpeg';
+    case 'js':
+        return 'application/javascript';
+    default:
+        return 'text/plain';
+    }
+}
+
+function try_file($path) {
+    if (!file_exists($path))
+        return false;
+
+    $info = pathinfo($path);
+    $ext = @$info['extension'] ?: null;
+    header('Content-Type: ' . content_type($ext));
+    readfile($path);
+    return true;
 }
 
 // Contournement de bug dans le serveur embarqué de PHP
 $_SERVER['SCRIPT_NAME'] = substr($_SERVER['SCRIPT_FILENAME'], strlen($_SERVER['DOCUMENT_ROOT']));
+$uri = urldecode($_SERVER["REQUEST_URI"]);
 
-// Fichiers /static/
-if (file_exists('./'.$_SERVER['REQUEST_URI'])) return false;
+if (preg_match('/\.(?:css|gif|ico|jpeg|jpg|js|png|ttf)$/', $uri)) {
+    // Fichiers /static/
+    if (try_file('.' . $uri) === true) return true;
 
-// Fichiers /data/
-$root = getenv('STRASS_ROOT') or 'htdocs/';
-if (file_exists($root.$_SERVER['REQUEST_URI'])) {
-  header('Content-Type: '.content_type($_SERVER['REQUEST_URI']));
-  readfile($root.$_SERVER['REQUEST_URI']);
-  return;
+    // Fichiers /data/
+    $root = getenv('STRASS_ROOT') or 'htdocs';
+    if (try_file($root . $uri) === true) return true;
+
+    header("HTTP/1.0 404 Not Found");
+    error_log("Pas trouvé : " . $path);
+    return true;
 }
 
-if (strpos($_SERVER['REQUEST_URI'], 'favicon.ico')) return false;
-
-// Le reste
-include_once 'index.php';
+/* Passer la main à index.php. */
+return false;
