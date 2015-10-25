269a2798f9d7d20054935160b9dc0407e10f3954
Étienne BERSAC <bersace03@gmail.com>
2015-10-25 11:56:05 +0100

    Corriger la recherche de fichier à autocharger.

--- include/Wtk.php
+++ include/Wtk.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'Wtk/Utils.php';
 
+/* C'est sale, car Wtk ne cherche que dans son dossier include. En même temps
+ * voilà quoi. */
+define('WTK_INCLUDE_DIR', dirname(__FILE__));
+
 abstract class Wtk
 {
 	public static function init()
@@ -13,7 +17,8 @@ abstract class Wtk
 		$file = str_replace ('_', '/', $class).'.php';
 		$exists = false;
 
-		if (file_exists('include/'.$file))
+
+		if (file_exists(WTK_INCLUDE_DIR . DIRECTORY_SEPARATOR . $file))
 			include_once $file;
 	}
 }
