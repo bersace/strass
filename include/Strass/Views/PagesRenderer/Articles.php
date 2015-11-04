a411ae14dc4d0b25e38d50b707558e48be702901
Étienne BERSAC <bersace03@gmail.com>
2015-11-04 20:29:44 +0100

    Réindenter S/V/PR/Articles.php

--- include/Strass/Views/PagesRenderer/Articles.php
+++ include/Strass/Views/PagesRenderer/Articles.php
@@ -2,31 +2,30 @@
 
 class Strass_Views_PagesRenderer_Articles extends Wtk_Pages_Renderer
 {
-  protected $view;
+    protected $view;
 
-  function __construct($view)
-  {
-    $href = $view->url(array('page' => '%i'));
-    parent::__construct(urldecode($href),
-			true,
-			array('previous' => "Précédents",
-			      'next' => "Suivants"));
-    $this->view = $view;
-  }
+    function __construct($view)
+    {
+        $href = $view->url(array('page' => '%i'));
+        parent::__construct(urldecode($href), true, array(
+            'previous' => "Précédents",
+            'next' => "Suivants"));
+        $this->view = $view;
+    }
 
-  function render($id, $article, $root)
-  {
-    $s = $root->addSection($article->slug, $this->view->lienArticle($article));
-    $s->addFlags('article');
-    $s->addChild($this->view->vignetteIndividu($article->findAuteur())->addFlags('mini'));
+    function render($id, $article, $root)
+    {
+        $s = $root->addSection($article->slug, $this->view->lienArticle($article));
+        $s->addFlags('article');
+        $s->addChild($this->view->vignetteIndividu($article->findAuteur())->addFlags('mini'));
 
-    $contenu = $s->addSection()->addFlags('contenu');
+        $contenu = $s->addSection()->addFlags('contenu');
 
-    $t = $contenu->addText(wtk_first_lines($article->getBoulet() . "\n\n". $article->article));
-    $tw = $t->getTextWiki();
-    $tw->setRenderConf('Xhtml', 'image', 'base', $article->getDossier());
-    $contenu->addParagraph($this->view->signature($article), ".")->addFlags('signature');
-    $lien = $this->view->lienArticle($article, 'Lire la suite…');
-    $contenu->addParagraph($lien)->addFlags('suite');
-  }
+        $t = $contenu->addText(wtk_first_lines($article->getBoulet() . "\n\n". $article->article));
+        $tw = $t->getTextWiki();
+        $tw->setRenderConf('Xhtml', 'image', 'base', $article->getDossier());
+        $contenu->addParagraph($this->view->signature($article), ".")->addFlags('signature');
+        $lien = $this->view->lienArticle($article, 'Lire la suite…');
+        $contenu->addParagraph($lien)->addFlags('suite');
+    }
 }
