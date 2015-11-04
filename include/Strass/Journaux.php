4a7d64a868b484ae72cff43c8d1208e54066fb10
Étienne BERSAC <bersace03@gmail.com>
2015-11-04 18:40:56 +0100

    Réindenter S/Journaux.php

--- include/Strass/Journaux.php
+++ include/Strass/Journaux.php
@@ -4,271 +4,282 @@ require_once 'Strass/Commentaires.php';
 
 class Journaux extends Strass_Db_Table_Abstract
 {
-  protected $_name = 'journal';
-  protected $_rowClass = 'Journal';
-  protected $_referenceMap = array('Unite' => array('columns' => 'unite',
-						    'refTableClass' => 'Unites',
-						    'refColumns' => 'id',
-						    'onUpdate' => self::CASCADE,
-						    'onDelete' => self::CASCADE));
+    protected $_name = 'journal';
+    protected $_rowClass = 'Journal';
+    protected $_referenceMap = array(
+        'Unite' => array(
+            'columns' => 'unite',
+            'refTableClass' => 'Unites',
+            'refColumns' => 'id',
+            'onUpdate' => self::CASCADE,
+            'onDelete' => self::CASCADE));
 }
 
 class Journal extends Strass_Db_Table_Row_Abstract implements Zend_Acl_Resource_Interface
 {
-  protected $_privileges = array(array('chef',		null),
-				 array('assistant',	null),
-				 array('membre', 'ecrire'));
+    protected $_privileges = array(
+        array('chef',		null),
+        array('assistant',	null),
+        array('membre', 'ecrire'));
 
-  function getResourceId()
-  {
-    return 'journal-'.$this->slug;
-  }
-
-  function initAclResource(&$acl)
-  {
-    $acl->add(new Zend_Acl_Resource($this->getResourceId()));
-    $u = $this->findParentUnites();
-    $this->initPrivileges($acl, array($u));
-    // permettre à un scout d'écrire dans le blog de sa troupe
-    foreach($u->findSousUnites() as $u)
-      $acl->allow($u->getRoleId('membre'), $this, 'ecrire');
-  }
-
-  function __toString()
-  {
-    return $this->nom;
-  }
-
-  function getDossier()
-  {
-    return 'data/journaux/' . $this->slug;
-  }
-
-  function selectArticles()
-  {
-    $t = new Articles;
-    $s = $t->select()
-      ->setIntegrityCheck(false)
-      ->from('article')
-      ->where('article.journal = ?', $this->id);
-    return $s;
-  }
-
-  function findArticles($select = null)
-  {
-    $t = new Articles;
-
-    if (is_string($select)) {
-      $select = $t->select()
-	->where($select);
+    function getResourceId()
+    {
+        return 'journal-'.$this->slug;
     }
 
-    $select->setIntegrityCheck(false)
-      ->from('article')
-      ->where('article.journal = ?', $this->id);
+    function initAclResource(&$acl)
+    {
+        $acl->add(new Zend_Acl_Resource($this->getResourceId()));
+        $u = $this->findParentUnites();
+        $this->initPrivileges($acl, array($u));
+        // permettre à un scout d'écrire dans le blog de sa troupe
+        foreach($u->findSousUnites() as $u)
+            $acl->allow($u->getRoleId('membre'), $this, 'ecrire');
+    }
 
-    return $t->fetchAll($select);
-  }
+    function __toString()
+    {
+        return $this->nom;
+    }
+
+    function getDossier()
+    {
+        return 'data/journaux/' . $this->slug;
+    }
+
+    function selectArticles()
+    {
+        $t = new Articles;
+        $s = $t->select()
+               ->setIntegrityCheck(false)
+               ->from('article')
+               ->where('article.journal = ?', $this->id);
+        return $s;
+    }
+
+    function findArticles($select = null)
+    {
+        $t = new Articles;
+
+        if (is_string($select)) {
+            $select = $t->select()
+                        ->where($select);
+        }
+
+        $select->setIntegrityCheck(false)
+               ->from('article')
+               ->where('article.journal = ?', $this->id);
+
+        return $t->fetchAll($select);
+    }
 }
 
 
 class Etiquettes extends Zend_Db_Table_Abstract
 {
-  protected $_name = 'article_etiquette';
-  protected $_referenceMap = array('Articles' => array('columns' => 'article',
-						       'refTableClass' => 'Articles',
-						       'refColumns' => 'id',
-						       'onUpdate' => self::CASCADE,
-						       'onDelete' => self::CASCADE));
+    protected $_name = 'article_etiquette';
+    protected $_referenceMap = array(
+        'Articles' => array(
+            'columns' => 'article',
+            'refTableClass' => 'Articles',
+            'refColumns' => 'id',
+            'onUpdate' => self::CASCADE,
+            'onDelete' => self::CASCADE));
 }
 
 
 class Articles extends Strass_Db_Table_Abstract
 {
-  protected $_name = 'article';
-  protected $_rowClass = 'Article';
-  protected $_dependentTables = array('Etiquettes');
-  protected $_referenceMap = array('Journal' => array('columns' => 'journal',
-						      'refTableClass' => 'Journaux',
-						      'refColumns' => 'id',
-						      'onUpdate' => self::CASCADE,
-						      'onDelete' => self::CASCADE),
-				   'Description' => array('columns' => 'commentaires',
-							  'refTableClass' => 'Commentaires',
-							  'refColumns' => 'id',
-							  'onUpdate' => self::CASCADE,
-							  'onDelete' => self::CASCADE));
+    protected $_name = 'article';
+    protected $_rowClass = 'Article';
+    protected $_dependentTables = array('Etiquettes');
+    protected $_referenceMap = array(
+        'Journal' => array(
+            'columns' => 'journal',
+            'refTableClass' => 'Journaux',
+            'refColumns' => 'id',
+            'onUpdate' => self::CASCADE,
+            'onDelete' => self::CASCADE),
+        'Description' => array(
+            'columns' => 'commentaires',
+            'refTableClass' => 'Commentaires',
+            'refColumns' => 'id',
+            'onUpdate' => self::CASCADE,
+            'onDelete' => self::CASCADE));
 
-  function fetchAll($where = NULL, $order = NULL, $count = NULL, $offset = NULL)
-  {
-    $args = func_get_args();
-    if ($args && $args[0] instanceof Zend_Db_Table_Select) {
-      $args[0] = clone $args[0];
-      $this->_ordonner($args[0]);
+    function fetchAll($where=NULL, $order=NULL, $count=NULL, $offset=NULL)
+    {
+        $args = func_get_args();
+        if ($args && $args[0] instanceof Zend_Db_Table_Select) {
+            $args[0] = clone $args[0];
+            $this->_ordonner($args[0]);
+        }
+        return call_user_func_array(array('parent', 'fetchAll'), $args);
     }
-    return call_user_func_array(array('parent', 'fetchAll'), $args);
-  }
 
-  protected function _ordonner($select)
-  {
-    $select->distinct()
-      ->join(array('strass_article_ordre' => 'commentaire'),
-	     'strass_article_ordre.id = article.commentaires'."\n", array())
-      ->order('strass_article_ordre.date DESC');
-  }
+    protected function _ordonner($select)
+    {
+        $select->distinct()
+               ->join(
+                   array('strass_article_ordre' => 'commentaire'),
+                   'strass_article_ordre.id = article.commentaires'."\n",
+                   array())
+               ->order('strass_article_ordre.date DESC');
+    }
 }
 
 class Article extends Strass_Db_Table_Row_Abstract implements Zend_Acl_Resource_Interface
 {
-  protected $_tableClass = 'Articles';
-  protected $_privileges = array(array('chef', NULL),
-				 array('assistant', NULL));
+    protected $_tableClass = 'Articles';
+    protected $_privileges = array(
+        array('chef', NULL),
+        array('assistant', NULL));
 
-  function getResourceId()
-  {
-    return 'article-'.$this->slug;
-  }
-
-  function initAclResource(&$acl)
-  {
-    $acl->add(new Zend_Acl_Resource($this->getResourceId()));
-    $this->initPrivileges($acl, array($this->findUnite()));
-    // permettre à l'auteur d'éditer ou de supprimer son
-    // article (pas de le publier, ça relève du poste dans
-    // l'unité).
-    $auteur = $this->findAuteur();
-    if ($acl->hasRole($auteur))
-      $acl->allow($auteur, $this,
-		  array('editer', 'supprimer'));
-
-    if ($this->public)
-      $acl->allow(null, $this, 'voir');
-  }
-
-  function getDossier($data = null)
-  {
-    $j = $this->findParentJournaux();
-    $r = $j->getDossier();
-    $data = $data ? $data : $this->_data;
-    return $r . '/' . $data['slug'] . '/';
-  }
-
-  function storeImage($tmp, $name)
-  {
-    $dossier = $this->getDossier();
-    if (!is_readable($dossier))
-      mkdir($dossier, 0750, true);
-
-    $target = $dossier.$name;
-    if (!move_uploaded_file($tmp, $target)) {
-      throw new Exception("Impossible de récupérer l'image");
+    function getResourceId()
+    {
+        return 'article-'.$this->slug;
     }
-  }
 
-  function renameImage($from, $to)
-  {
-    rename($this->getDossier().$from, $this->getDossier().$to);
-  }
+    function initAclResource(&$acl)
+    {
+        $acl->add(new Zend_Acl_Resource($this->getResourceId()));
+        $this->initPrivileges($acl, array($this->findUnite()));
+        // permettre à l'auteur d'éditer ou de supprimer son
+        // article (pas de le publier, ça relève du poste dans
+        // l'unité).
+        $auteur = $this->findAuteur();
+        if ($acl->hasRole($auteur))
+            $acl->allow(
+                $auteur, $this, array('editer', 'supprimer'));
 
-  function deleteImage($name)
-  {
-    $path = $this->getDossier().$name;
-    if (file_exists($path))
-      unlink($path);
-  }
-
-  function getImages()
-  {
-    $dossier = $this->getDossier();
-    $fichiers = (array) @scandir($dossier);
-    $images = array();
-    foreach($fichiers as $fichier) {
-      if ($fichier != '.' && $fichier != '..') {
-	$images[] = $fichier;
-      }
+        if ($this->public)
+            $acl->allow(null, $this, 'voir');
     }
-    return array_filter($images);
-  }
 
-  function getDate()
-  {
-    return $this->findParentCommentaires()->date;
-  }
+    function getDossier($data = null)
+    {
+        $j = $this->findParentJournaux();
+        $r = $j->getDossier();
+        $data = $data ? $data : $this->_data;
+        return $r . '/' . $data['slug'] . '/';
+    }
 
-  function getBoulet($generate=false)
-  {
-    if ($this->boulet)
-      return $this->boulet;
-    else
-      return $this->article;
-  }
+    function storeImage($tmp, $name)
+    {
+        $dossier = $this->getDossier();
+        if (!is_readable($dossier))
+            mkdir($dossier, 0750, true);
 
-  function findAuteur()
-  {
-    $t = new Individus;
-    $s = $t->select()
-      ->setIntegrityCheck(false)
-      ->from('individu')
-      ->join('commentaire', 'commentaire.auteur = individu.id', array())
-      ->join('article', 'article.commentaires = commentaire.id', array())
-      ->where('article.id = ?', $this->id);
+        $target = $dossier.$name;
+        if (!move_uploaded_file($tmp, $target)) {
+            throw new Exception("Impossible de récupérer l'image");
+        }
+    }
 
-    return $t->fetchOne($s);
-  }
+    function renameImage($from, $to)
+    {
+        rename($this->getDossier().$from, $this->getDossier().$to);
+    }
 
-  function findUnite()
-  {
-    $t = new Unites;
-    $s = $t->select()
-      ->setIntegrityCheck(false)
-      ->from('unite')
-      ->join('journal', 'journal.unite = unite.id', array())
-      ->where('journal.id = ?', $this->journal);
-    return $t->fetchOne($s);
-  }
+    function deleteImage($name)
+    {
+        $path = $this->getDossier().$name;
+        if (file_exists($path))
+            unlink($path);
+    }
 
-  function __toString()
-  {
-    return $this->titre;
-  }
+    function getImages()
+    {
+        $dossier = $this->getDossier();
+        $fichiers = (array) @scandir($dossier);
+        $images = array();
+        foreach($fichiers as $fichier) {
+            if ($fichier != '.' && $fichier != '..') {
+                $images[] = $fichier;
+            }
+        }
+        return array_filter($images);
+    }
+
+    function getDate()
+    {
+        return $this->findParentCommentaires()->date;
+    }
+
+    function getBoulet($generate=false)
+    {
+        if ($this->boulet)
+            return $this->boulet;
+        else
+            return $this->article;
+    }
+
+    function findAuteur()
+    {
+        $t = new Individus;
+        $s = $t->select()
+               ->setIntegrityCheck(false)
+               ->from('individu')
+               ->join('commentaire', 'commentaire.auteur = individu.id', array())
+               ->join('article', 'article.commentaires = commentaire.id', array())
+               ->where('article.id = ?', $this->id);
+
+        return $t->fetchOne($s);
+    }
+
+    function findUnite()
+    {
+        $t = new Unites;
+        $s = $t->select()
+               ->setIntegrityCheck(false)
+               ->from('unite')
+               ->join('journal', 'journal.unite = unite.id', array())
+               ->where('journal.id = ?', $this->journal);
+        return $t->fetchOne($s);
+    }
+
+    function __toString()
+    {
+        return $this->titre;
+    }
 
 
-  function _postUpdate()
-  {
-    $from = $this->getDossier($this->_cleanData);
-    $to = $this->getDossier();
-    if ($from != $to && is_readable($from)) {
-      if (!rename($from, $to))
-	throw new Exception("Impossible de renomer le dossier ".$from.
+    function _postUpdate()
+    {
+        $from = $this->getDossier($this->_cleanData);
+        $to = $this->getDossier();
+        if ($from != $to && is_readable($from)) {
+            if (!rename($from, $to))
+                throw new Exception("Impossible de renomer le dossier ".$from.
 			    " en ".$to);
+        }
+
+        // suppression de dossier vide.
+        $p = dirname($from);
+        $fs = scandir($p);
+        if (count($fs) == 2) {
+            rmdir($p);
+        }
     }
 
-    // suppression de dossier vide.
-    $p = dirname($from);
-    $fs = scandir($p);
-    if (count($fs) == 2) {
-      rmdir($p);
-    }
-  }
+    function _postDelete()
+    {
+        $dossier = $this->getDossier();
+        $fichiers = $this->getImages();
 
-  function _postDelete()
-  {
-    $dossier = $this->getDossier();
-    $fichiers = $this->getImages();
-
-    foreach($fichiers as $fichier) {
-      $path = $dossier.'/'.$fichier;
-      if (!@unlink($path)) {
-	throw new Exception("Impossible de supprimer le fichier ".
+        foreach($fichiers as $fichier) {
+            $path = $dossier.'/'.$fichier;
+            if (!@unlink($path)) {
+                throw new Exception("Impossible de supprimer le fichier ".
 			    $fichier);
-      }
-    }
+            }
+        }
 
-    if (file_exists($dossier)) {
-      if (!@rmdir($dossier)) {
-	throw new Exception("Impossible de supprimer le dossier ".
+        if (file_exists($dossier)) {
+            if (!@rmdir($dossier)) {
+                throw new Exception("Impossible de supprimer le dossier ".
 			    $dossier);
-      }
+            }
+        }
     }
-  }
 }
