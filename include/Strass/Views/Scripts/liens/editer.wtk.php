090dbe111f306a165cdc31e7949bc53b7ca609f5
Étienne BERSAC <bersace03@gmail.com>
2015-11-15 11:42:00 +0100

    Utiliser le champs URL pour les liens

--- include/Strass/Views/Scripts/liens/editer.wtk.php
+++ include/Strass/Views/Scripts/liens/editer.wtk.php
@@ -1,10 +1,10 @@
 <?php
 
 $f = $this->document->addForm($this->model);
-$f->addTable('liens',
-	     array('url'		=> array('Entry', 24),
-		   'nom'		=> array('Entry', 24),
-		   'description'	=> array('Entry', 32, 3)));
+$f->addTable('liens', array(
+    'url' => array('Url', 24),
+    'nom' => array('Entry', 24),
+    'description' => array('Entry', 32, 2)));
 
 $b = $f->addForm_ButtonBox();
 $b->addForm_Submit($this->model->getSubmission('enregistrer'));
