d5e83a20df2f8ca7ec3ef3294ab7610358e3ea19
Étienne BERSAC <bersace03@gmail.com>
2015-11-15 11:21:42 +0100

    Remanier le champ recherche

--- include/Wtk/Form/Control/Search.php
+++ include/Wtk/Form/Control/Search.php
@@ -1,22 +1,5 @@
 <?php
 
-class Wtk_Form_Control_Search extends Wtk_Form_Control
+class Wtk_Form_Control_Search extends Wtk_Form_Control_Entry
 {
-	function __construct ($instance, $width = 12, $height = 1, $suffix = '')
-	{
-		parent::__construct ($instance);
-		$this->setSize ($width, $height);
-		$this->useSuffix($suffix);
-	}
-
-	function useSuffix($suffix)
-	{
-		$this->suffix = $suffix;
-	}
-
-	function setSize ($width, $height)
-	{
-		$this->width = is_null ($width) ? $this->width : $width;
-		$this->height = is_null ($height) ? $this->height : $height;
-	}
 }
