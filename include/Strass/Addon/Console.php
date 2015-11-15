682aa7265db08ea95abbd6c4207332123d619ef0
Étienne BERSAC <bersace03@gmail.com>
2015-11-15 10:56:30 +0100

    Réindenter S/A/Console.php

--- include/Strass/Addon/Console.php
+++ include/Strass/Addon/Console.php
@@ -2,31 +2,31 @@
 
 class Strass_Addon_Console extends Strass_Addon_Liens
 {
-  function __construct()
-  {
-    parent::__construct('console', 'Console');
-  }
-
-  function initView($view)
-  {
-    $view->auth_login_model = Zend_Registry::get('login_model');
-    $view->auth_logout_model = Zend_Registry::get('logout_model');
-
-    $acl = Zend_Registry::get('acl');
-    $user = Zend_Registry::get('user');
-
-    try {
-      $sudoer = Zend_Registry::get('sudoer');
-      $this->append('Redevenir '.$sudoer->username,
-    		    array('controller' => 'membres', 'action' => 'unsudo'));
+    function __construct()
+    {
+        parent::__construct('console', 'Console');
     }
-    catch (Zend_Exception $e) {}
 
-    parent::initView($view);
-  }
+    function initView($view)
+    {
+        $view->auth_login_model = Zend_Registry::get('login_model');
+        $view->auth_logout_model = Zend_Registry::get('logout_model');
 
-  public function viewScript()
-  {
-    return 'console';
-  }
+        $acl = Zend_Registry::get('acl');
+        $user = Zend_Registry::get('user');
+
+        try {
+            $sudoer = Zend_Registry::get('sudoer');
+            $this->append('Redevenir '.$sudoer->username,
+            array('controller' => 'membres', 'action' => 'unsudo'));
+        }
+        catch (Zend_Exception $e) {}
+
+        parent::initView($view);
+    }
+
+    public function viewScript()
+    {
+        return 'console';
+    }
 }
