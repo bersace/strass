1c8f54ba4473bd8bef57ffe4bf92ccd00f7667d4
Étienne BERSAC <bersace03@gmail.com>
2015-11-15 11:43:48 +0100

    Réindenter S/V/S/i/editer.wtk.php

--- include/Strass/Views/Scripts/individus/editer.wtk.php
+++ include/Strass/Views/Scripts/individus/editer.wtk.php
@@ -1,18 +1,18 @@
 <?php
 
 $this->document->setTitle(new Wtk_Container(new Wtk_Inline("Modifier la fiche de "),
-					    $this->lienIndividu($this->individu)));
+$this->lienIndividu($this->individu)));
 $f = $this->document->addForm($this->model);
 
 $g = $f->addForm_Fieldset('État civil');
 try {
-  // ces champs ne sont pas forcément présent, soit parce que
-  // seul l'admin peut le corriger, soit parce qu'il faut être
-  // sachem pour y avoir accès.
-  $g->addEntry('prenom', 24);
-  $g->addEntry('nom', 24);
-  $g->addDate('naissance', '%e/%m/%Y');
-  $g->addSelect('sexe');
+    // ces champs ne sont pas forcément présent, soit parce que
+    // seul l'admin peut le corriger, soit parce qu'il faut être
+    // sachem pour y avoir accès.
+    $g->addEntry('prenom', 24);
+    $g->addEntry('nom', 24);
+    $g->addDate('naissance', '%e/%m/%Y');
+    $g->addSelect('sexe');
 } catch(Exception $e){}
 
 $g->addFile('image');
@@ -21,16 +21,16 @@ $g->addFile('image');
 $g = $f->addForm_Fieldset("Scoutisme");
 
 try {
-  $g->addEntry('totem', 24);
+    $g->addEntry('totem', 24);
 } catch(Exception $e){}
 
 try {
-  $g->addSelect('etape', true);
+    $g->addSelect('etape', true);
 } catch(Exception $e){}
 
 // suppression si vide.
 if (!$g->count())
-  $f->removeChild($g);
+    $f->removeChild($g);
 
 // contacts
 $g = $f->addForm_Fieldset('Contacts');
@@ -38,16 +38,17 @@ $g->addEntry('adresse', 32, 2);
 $g->addEntry('fixe', 14);
 $g->addEntry('portable', 14);
 if ($this->individu->isMember()) {
-  $url = $this->url(array('controller' => 'membres',
-			  'action' => 'parametres',
-			  'membre' => $this->individu->findUser()->username), true, true);
-  /* Simuler un champ de formulaire, mais c'est un lien */
-  $g->addSection()->addFlags('control', 'entry')
-    ->addSpan()->addFlags('control', 'entry', 'input')
-    ->addInline("[".$url." Éditer l'adresse électronique]");
+    $url = $this->url(
+        array('controller' => 'membres',
+        'action' => 'parametres',
+        'membre' => $this->individu->findUser()->username), true, true);
+    /* Simuler un champ de formulaire, mais c'est un lien */
+    $g->addSection()->addFlags('control', 'entry')
+      ->addSpan()->addFlags('control', 'entry', 'input')
+      ->addInline("[".$url." Éditer l'adresse électronique]");
 }
 else {
-  $g->addEMail('adelec', 24);
+    $g->addEMail('adelec', 24);
 }
 
 $g = $f->addForm_Fieldset('Notes');
