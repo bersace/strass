4e9da749fa66d2cf9ec0ffc2bdd521071c9d356c
Étienne BERSAC <bersace03@gmail.com>
2015-11-15 11:29:25 +0100

    Réindenter S/V/S/m/recouvrir.wtk.php

--- include/Strass/Views/Scripts/membres/recouvrir.wtk.php
+++ include/Strass/Views/Scripts/membres/recouvrir.wtk.php
@@ -1,28 +1,28 @@
 <?php
 
 if ($this->finish) {
-  $this->document->setTitle(null);
-  $d = $this->document->addDialog("Nouveau mot de passe défini");
-  $d->addText(<<<EOS
+    $this->document->setTitle(null);
+    $d = $this->document->addDialog("Nouveau mot de passe défini");
+    $d->addText(<<<EOS
 
 Votre nouveau mot de passe est défini. Vous pouvez vous identifier avec \
 votre adresse {$this->individu->adelec} et ce mot de passe.
 
 Bonne visite !
 EOS
-);
+    );
 }
 else if ($this->set) {
-  $f = $this->document->addChild(new Wtk_Form($this->set));
-  $f->addPassword('nouveau', 24);
-  $f->addPassword('confirmation', 24);
-  $f->addForm_ButtonBox()->addForm_Submit($this->set->getSubmission('enregistrer'));
+    $f = $this->document->addChild(new Wtk_Form($this->set));
+    $f->addPassword('nouveau', 24);
+    $f->addPassword('confirmation', 24);
+    $f->addForm_ButtonBox()->addForm_Submit($this->set->getSubmission('enregistrer'));
 }
 else if ($this->send) {
-  $f = $this->document->addChild(new Wtk_Form($this->send));
-  $f->addSection()->addFlags('info')
-    ->addText("Un courriel va vous être envoyé avec un lien temporaire ".
-	      "vers la page pour définir un nouveau mot de passe.");
-  $f->addEntry('adelec', 24);
-  $f->addForm_ButtonBox()->addForm_Submit($this->send->getSubmission('envoyer'));
+    $f = $this->document->addChild(new Wtk_Form($this->send));
+    $f->addSection()->addFlags('info')
+      ->addText("Un courriel va vous être envoyé avec un lien temporaire ".
+      "vers la page pour définir un nouveau mot de passe.");
+    $f->addEntry('adelec', 24);
+    $f->addForm_ButtonBox()->addForm_Submit($this->send->getSubmission('envoyer'));
 }
