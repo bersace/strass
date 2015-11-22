bdd9d006dc6fc2747b3681e1a7d6f08336272148
Étienne BERSAC <bersace03@gmail.com>
2015-11-22 12:18:40 +0100

    Réindenter S/P/M/Photos.php

--- include/Strass/Pages/Model/Photos.php
+++ include/Strass/Pages/Model/Photos.php
@@ -2,10 +2,10 @@
 
 class Strass_Pages_Model_Photos extends Strass_Pages_Model_Historique
 {
-  /* Devrait-on initialiser la liste des années à partir des années où
-     on a des photos ? */
+    /* Devrait-on initialiser la liste des années à partir des années où
+       on a des photos ? */
 
-  function fetch($annee = NULL) {
-    return array('albums' => $this->unite->findAlbums($annee));
-  }
+    function fetch($annee = NULL) {
+        return array('albums' => $this->unite->findAlbums($annee));
+    }
 }
