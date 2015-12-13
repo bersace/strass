7d12243252e988155a404f888e092681bb0baec6
Étienne BERSAC <bersace03@gmail.com>
2015-12-13 17:40:36 +0100

    Réindenter S/V/H/VignetteUnite.php

--- include/Strass/Views/Helpers/VignetteUnite.php
+++ include/Strass/Views/Helpers/VignetteUnite.php
@@ -2,51 +2,54 @@
 
 class Strass_View_Helper_VignetteUnite
 {
-  protected	$view;
+    protected	$view;
 
-  public function setView($view)
-  {
-    $this->view = $view;
-  }
-
-  public function vignetteUnite($unite, $annee = null, $label = null, $urlOptions = array())
-  {
-    if (!$unite)
-      return;
-
-    $this->view->document->addStyleComponents('vignette');
-    $label = $label ? $label : $unite->getName();
-
-    if ($src = $unite->getCheminImage())
-      $image = new Wtk_Image($src, "Photo d'unité", $label);
-    else {
-      $photo = $unite->findPhotoAleatoire($annee);
-      if ($photo)
-	$image = new Wtk_Image($photo->getCheminVignette(),
-			       $photo->titre, $unite->getFullname());
-      else {
-	$image = new Wtk_Paragraph("Pas d'image !");
-	$image->addFlags('empty', 'image');
-      }
+    public function setView($view)
+    {
+        $this->view = $view;
     }
 
-    $urlOptions = array_merge(array('controller'	=> 'unites',
-				    'action'		=> 'index',
-				    'unite'		=> $unite->slug),
-			      $urlOptions);
-    $type = $unite->findParentTypesUnite();
-    $w = new Wtk_Section;
-    $w->addFlags('wrapper')->addChild($image);
-    $plabel = new Wtk_Paragraph($label);
-    $plabel->addFlags('label');
+    public function vignetteUnite($unite, $annee = null, $label = null, $urlOptions = array())
+    {
+        if (!$unite)
+            return;
 
-    $link = new Wtk_Link($this->view->url($urlOptions), $label,
-			 new Wtk_Container($w, $plabel));
-    $link->addFlags('vignette', $type->slug);
-	$link->addFlags('unite');
-    if (!$src)
-      $link->addFlags('photo');
+        $this->view->document->addStyleComponents('vignette');
+        $label = $label ? $label : $unite->getName();
 
-    return $link;
-  }
+        if ($src = $unite->getCheminImage())
+            $image = new Wtk_Image($src, "Photo d'unité", $label);
+        else {
+            $photo = $unite->findPhotoAleatoire($annee);
+            if ($photo)
+                $image = new Wtk_Image($photo->getCheminVignette(),
+                $photo->titre, $unite->getFullname());
+            else {
+                $image = new Wtk_Paragraph("Pas d'image !");
+                $image->addFlags('empty', 'image');
+            }
+        }
+
+        $urlOptions = array_merge(
+            array(
+                'controller' => 'unites',
+                'action' => 'index',
+                'unite' => $unite->slug),
+            $urlOptions);
+        $type = $unite->findParentTypesUnite();
+        $w = new Wtk_Section;
+        $w->addFlags('wrapper')->addChild($image);
+        $plabel = new Wtk_Paragraph($label);
+        $plabel->addFlags('label');
+
+        $link = new Wtk_Link(
+            $this->view->url($urlOptions), $label,
+            new Wtk_Container($w, $plabel));
+        $link->addFlags('vignette', $type->slug);
+        $link->addFlags('unite');
+        if (!$src)
+            $link->addFlags('photo');
+
+        return $link;
+    }
 }
