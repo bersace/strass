79b2f435a25aabb18b3c0d870dc951bf72059945
Étienne BERSAC <bersace03@gmail.com>
2015-12-13 17:08:48 +0100

    Réindenter S/Cache.php

--- include/Strass/Cache.php
+++ include/Strass/Cache.php
@@ -3,41 +3,43 @@
 class Strass_Cache
 {
 
-  static function setup()
-  {
-    $cachedir = 'private/cache';
-    if (!file_exists($cachedir))
-      mkdir($cachedir, 0700, true);
-    Zend_Registry::set('cache',
-		       Zend_Cache::factory('Core', 'Strass_Cache_Backend_File',
-					   array('automatic_serialization' => true),
-					   array('cache_dir' => $cachedir),
-					   false, /* custom frontend */
-					   true /* custom backend */
-					   ));
-  }
+    static function setup()
+    {
+        $cachedir = 'private/cache';
+        if (!file_exists($cachedir))
+            mkdir($cachedir, 0700, true);
+        Zend_Registry::set(
+            'cache',
+            Zend_Cache::factory(
+                'Core', 'Strass_Cache_Backend_File',
+                array('automatic_serialization' => true),
+                array('cache_dir' => $cachedir),
+                false, /* custom frontend */
+                true /* custom backend */
+            ));
+    }
 
-  function __construct()
-  {
-    $this->data = array();
-  }
-
-
-  function has($id)
-  {
-    return array_key_exists($id, $this->data);
-  }
+    function __construct()
+    {
+        $this->data = array();
+    }
 
 
-  function load($id)
-  {
-    if (!$this->has($id))
-      return false;
-    return $this->data[$id];
-  }
+    function has($id)
+    {
+        return array_key_exists($id, $this->data);
+    }
 
-  function save($value, $id)
-  {
-    $this->data[$id] = $value;
-  }
+
+    function load($id)
+    {
+        if (!$this->has($id))
+            return false;
+        return $this->data[$id];
+    }
+
+    function save($value, $id)
+    {
+        $this->data[$id] = $value;
+    }
 }
