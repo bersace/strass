367782baa5d907df657232aee5f07f789c037186
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-10-13 14:21:49 +0200

    Assigner uniquement data/ et private/
    C'est pour faire un peu mieux cohabiter le conteneur et l'extérieur.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
 # S'assurer que le volume est accessible à l'utilisateur strass.
-chown -R strass: ${STRASS_ROOT}
+chmod 0770 ${STRASS_ROOT}
+chown -R strass: ${STRASS_ROOT}/data ${STRASS_ROOT}/private ||:
 
 exec ${@-/usr/sbin/php5-fpm --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf}
