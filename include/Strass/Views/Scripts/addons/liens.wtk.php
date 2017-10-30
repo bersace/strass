a941c75efbc5449c20b3df30bd078cf77dcef36d
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-10-30 16:40:38 +0100

    Permettre d'insérer une URL forgées dans les annexes

--- include/Strass/Views/Scripts/addons/liens.wtk.php
+++ include/Strass/Views/Scripts/addons/liens.wtk.php
@@ -5,7 +5,10 @@ if (count($this->liens)) {
     $list = $connexes->addList();
     $current_uri = Zend_Controller_Front::getInstance()->getRequest()->REQUEST_URI;
     foreach ($this->liens as $lien) {
-        $url = $this->url($lien['urlOptions'], $lien['reset'], false, $lien['anchor']);
+        if (is_array($lien['urlOptions']))
+            $url = $this->url($lien['urlOptions'], $lien['reset'], false, $lien['anchor']);
+        else
+            $url = $lien['urlOptions'];
         $i = $list->addItem();
         $i->addLink($url, new Wtk_Metas($lien['metas']));
         $i->addFlags($lien['urlOptions'], split('/', $url), $lien['flags']);
