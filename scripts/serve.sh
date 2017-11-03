503a912ecc0095aa360fbc6ca6fbfce6f50ed2d9
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-11-03 21:19:32 +0100

    Aligner upload_max_filesize en dév

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -6,6 +6,7 @@ cd $(readlink -m $0/../..)
 export STRASS_MODE=devel
 exec php -S 0.0.0.0:${SERVE_PORT} \
     -d include_path=${PWD}/include/ \
+    -d upload_max_filesize=10M \
     -d xdebug.profiler_output_dir=${PWD} \
     -d xdebug.profiler_enable_trigger=1 \
     index.php
