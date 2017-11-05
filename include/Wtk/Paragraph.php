a7024ba20c0543684810ed03e64684e129f0b248
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-11-05 19:40:19 +0100

    Golf de la classe paragraph

--- include/Wtk/Paragraph.php
+++ include/Wtk/Paragraph.php
@@ -5,6 +5,7 @@ class Wtk_Paragraph extends Wtk_Container
 	function __construct($text = '')
 	{
 		parent::__construct();
+        unset($this->flags[0]);
 		$children = func_get_args();
 		call_user_func_array(array(&$this, 'addChildren'),
 				     $children);
